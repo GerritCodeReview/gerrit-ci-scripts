54c9b41970cf9c8c98e828252f63949ac2051f3c
Luca Milanesio <luca.milanesio@gmail.com>
2015-10-15 15:48:09 +0100

    Align the Gerrit master build with the latest buck behaviour
    Since the upgrade to the latest Buck build update the Gerrit master
    build was broken on the CI. This is due to the slightly different
    way that the gen files gets created.
    
    Change-Id: I054220c1ccdb780e42595a0716c62b5c84815fdb

--- jenkins/gerrit-buck-build.sh
+++ jenkins/gerrit-buck-build.sh
@@ -2,4 +2,5 @@ rm -Rf buck-out
 find plugins -type l -exec rm -f {} \;
 export BUCK_CLEAN_REPO_IF_DIRTY=y
 buck build -v 3 api api_install plugins:core release
-cp buck-out/gen/release.war buck-out/gen/gerrit.war
+export WAR=$(find buck-out/gen -name 'release.war')
+mv $WAR $(dirname $WAR)/gerrit.war
