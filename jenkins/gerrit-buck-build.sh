bc2ca0e3fcb93d70af54d6c0cf0acaf6fb0c7a8b
Luca Milanesio <luca.milanesio@gmail.com>
2015-10-27 23:07:23 +0000

    Makes buck build compatible with stable-2.10
    At the time when Gerrit 2.10 was developed, Buck had the show-output
    command specified with an underscore.
    
    Change-Id: I5a5f0a14bfd8e3debee8147b8d89f6e1a0b57cd1

--- jenkins/gerrit-buck-build.sh
+++ jenkins/gerrit-buck-build.sh
@@ -2,4 +2,4 @@ rm -Rf buck-out
 find plugins -type l -exec rm -f {} \;
 export BUCK_CLEAN_REPO_IF_DIRTY=y
 buck build -v 3 api api_install plugins:core release
-mv $(buck targets --show-output release | awk '{print $2}') buck-out/gen/gerrit.war
+mv $(buck targets --show_output release | awk '{print $2}') buck-out/gen/gerrit.war
