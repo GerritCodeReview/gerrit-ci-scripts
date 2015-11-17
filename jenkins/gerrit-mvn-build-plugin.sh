2fa5b46c4b474bcd7f9f43ea8722d4a641ddf40c
Luca Milanesio <luca.milanesio@gmail.com>
2015-11-17 22:56:26 +0000

    Fixes plugins version extraction for Maven builds
    Change-Id: I70ac161cda2eed5ec28fbc33d996487fb1a609a3

--- jenkins/gerrit-mvn-build-plugin.sh
+++ jenkins/gerrit-mvn-build-plugin.sh
@@ -2,8 +2,11 @@ find . -type d -name 'target' -delete
 mvn package
 
 # Extract version information
-PLUGIN_JAR=$(ls target/{name}*jar)
-jar xf $PLUGIN_JAR META-INF/MANIFEST.MF
-PLUGIN_VERSION=$(grep "Implementation-Version" META-INF/MANIFEST.MF | cut -d ' ' -f 2)
+PLUGIN_JARS=$(find . -name '{repo}*jar')
+for jar in $PLUGIN_JARS
+do
+  jar xf $jar META-INF/MANIFEST.MF
+  PLUGIN_VERSION=$(grep "Implementation-Version" META-INF/MANIFEST.MF | cut -d ' ' -f 2)
 
-echo "$PLUGIN_VERSION" > $PLUGIN_JAR-version
+  echo "$PLUGIN_VERSION" > $jar-version
+done
