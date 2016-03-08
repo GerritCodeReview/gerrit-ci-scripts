986c1f0992be9a0853952c0079878d5d295a4edf
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2016-03-08 23:28:06 +0100

    Add manual jobs for gitiles on various branches
    This allows to backport a specific version of gitiles to the plugin
    generating all sets of manual jobs for master and each release
    
    Change-Id: Id3f61130ade1da3b0530210066b9aaecb21dbc69

---
+++ jenkins/gerrit-buck-build-plugin-manual.sh
@@ -0,0 +1,26 @@
+# NOTE: All Gerrit plugins currently depend on, at a minimum
+# the bucklets repo, and most plugins don't build outside the
+# Gerrit tree, even with bucklets.  As a result, we need to
+# graft the plugin onto the Gerrit repo.  We do this by checking
+# out the proper version of Gerrit, removing the plugin if it
+# exists, then use git read-tree to put the plugin we're
+# building in place.
+git checkout gerrit/{branch}
+rm -rf plugins/{name}
+git fetch https://gerrit.googlesource.com/plugins/{name} $REFS_CHANGE
+git read-tree -u --prefix=plugins/{name} FETCH_HEAD
+
+rm -Rf buck-out
+export BUCK_CLEAN_REPO_IF_DIRTY=y
+TARGETS=$(echo "{targets}" | sed -e 's/{{name}}/{name}/g')
+
+buck build -v 3 $TARGETS
+
+for JAR in $(buck targets --show_output $TARGETS | awk '{{print $2}}')
+do
+    jar xf $JAR META-INF/MANIFEST.MF
+    PLUGIN_VERSION=$(grep "Implementation-Version" META-INF/MANIFEST.MF | cut -d ' ' -f 2)
+    DEST_JAR=buck-out/gen/plugins/{name}/$(basename $JAR)
+    [ "$JAR" -ef "$DEST_JAR" ] || mv $JAR $DEST_JAR
+    echo "$PLUGIN_VERSION" > buck-out/gen/plugins/{name}/$(basename $JAR-version)
+done
