d31b91b70e2459df3637955f3414e453b0602e71
Luca Milanesio <luca.milanesio@gmail.com>
2016-05-04 11:38:14 +0100

    Fix install of Gerrit plugin api/ext to local Maven repo
    Change-Id: I26e65c9ca77c94f1dbb1a9e9ce97e9819376a95b

--- jenkins/gerrit-buck-build.sh
+++ jenkins/gerrit-buck-build.sh
@@ -1,5 +1,6 @@
 rm -Rf buck-out
 find plugins -type l -exec rm -f {} \;
 export BUCK_CLEAN_REPO_IF_DIRTY=y
-buck build -v 3 api api_install plugins:core release
+buck build -v 3 api plugins:core release
+sh tools/maven/api.sh install || true
 mv $(buck targets --show_output release | awk '{print $2}') buck-out/gen/gerrit.war
