6b7aa7a042776836fdf7aa9650b81f38cd755ec4
Luca Milanesio <luca.milanesio@gmail.com>
2016-05-17 13:26:08 +0100

    Consider exit value from api/ext install to local Maven repo
    Change-Id: I1f69cede37043adaede804a1cb573d716ea528fa

--- jenkins/gerrit-buck-build.sh
+++ jenkins/gerrit-buck-build.sh
@@ -2,5 +2,5 @@ rm -Rf buck-out
 find plugins -type l -exec rm -f {} \;
 export BUCK_CLEAN_REPO_IF_DIRTY=y
 buck build -v 3 api plugins:core release
-sh tools/maven/api.sh install || true
+sh tools/maven/api.sh install
 mv $(buck targets --show_output release | awk '{print $2}') buck-out/gen/gerrit.war
