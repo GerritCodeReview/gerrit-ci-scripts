2abbf6e04b2634cc89962659e9c9e45360c77a9b
Luca Milanesio <luca.milanesio@gmail.com>
2016-05-24 10:34:45 +0100

    Resume api install for Gerrit <= 2.12
    Change-Id: I4d8c141422f9627a61ccc87fb44f73c5766514b6

--- jenkins/gerrit-buck-build.sh
+++ jenkins/gerrit-buck-build.sh
@@ -2,5 +2,13 @@ rm -Rf buck-out
 find plugins -type l -exec rm -f {} \;
 export BUCK_CLEAN_REPO_IF_DIRTY=y
 buck build -v 3 api plugins:core release
-sh tools/maven/api.sh install
+
+if [ -f tools/maven/api.sh ]
+then
+  # From Gerrit 2.13 onwards
+  sh tools/maven/api.sh install
+else
+  # Up to Gerrit 2.12
+  buck build -v 3 api_install
+fi
 mv $(buck targets --show_output release | awk '{print $2}') buck-out/gen/gerrit.war
