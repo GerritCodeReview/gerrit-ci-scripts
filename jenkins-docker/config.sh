1a633e8a15d98dca8c7e9cb7468dbcff918f6376
Luca Milanesio <luca.milanesio@gmail.com>
2016-05-26 12:04:07 -0400

    Use standard XSLT processing for editing config.xml
    Instead of custom sed replacements, use standard XSLT processing
    and make the config.xml more configurable using Docker ENV
    properties.
    
    Default is without GitHub OAuth, to allow an easier out-of-the-box
    use of the VM for others.
    
    Change-Id: Ie9ae294cfdd7cb8f360acfbaa8337b3550b02b62

--- jenkins-docker/config.sh
+++ jenkins-docker/config.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-sed -i -e "s/#OAUTH_ID#/$OAUTH_ID/g" $JENKINS_REF/config.xml
-sed -i -e "s/#OAUTH_SECRET#/$OAUTH_SECRET/g" $JENKINS_REF/config.xml
-
 sed -i -e "s/#JENKINS_API_USER#/$JENKINS_API_USER/g" /etc/jenkins_jobs/jenkins_jobs.ini
 sed -i -e "s/#JENKINS_API_PASSWORD#/$JENKINS_API_PASSWORD/g" /etc/jenkins_jobs/jenkins_jobs.ini
