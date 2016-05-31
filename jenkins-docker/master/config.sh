52028c31f5822d3528c34c06edbfda684aa9630d
Luca Milanesio <luca.milanesio@gmail.com>
2016-05-31 17:13:46 -0400

    Add Docker plugin for using virtualised slaves
    We need to have docker packages installed in the VM
    and be root in order to be able to control docker host.
    In this way Jenkins can spawn new docker slaves to allow
    parallel executions of builds on dynamically provisioned
    docker machines.
    
    Change-Id: If428a202fdeacb6fd11a4fcaf72054dd064c54b8

--- jenkins-docker/master/config.sh
+++ jenkins-docker/master/config.sh
@@ -11,8 +11,12 @@ xsltproc \
   --stringparam use-security $USE_SECURITY \
   --stringparam oauth-client-id $OAUTH_ID \
   --stringparam oauth-client-secret $OAUTH_SECRET \
+  --stringparam docker-url $DOCKER_HOST \
   $JENKINS_REF/edit-config.xslt $CONFIG > /tmp/config.xml.new
 mv /tmp/config.xml.new $CONFIG
 
 sed -i -e "s/user=.*/user=$JENKINS_API_USER/" /etc/jenkins_jobs/jenkins_jobs.ini
 sed -i -e "s/password=.*/password=$JENKINS_API_PASSWORD/" /etc/jenkins_jobs/jenkins_jobs.ini
+
+cp -R $JENKINS_REF/.ssh ~/.
+chmod 600 ~/.ssh/id_rsa
