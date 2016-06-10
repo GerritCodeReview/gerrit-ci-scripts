933920645698933df0d2c5499c17d069e8be6855
Luca Milanesio <luca.milanesio@gmail.com>
2016-06-10 09:48:19 +0100

    Execute api.sh using bash
    api.sh uses bash syntax and thus needs to be executed
    using bash. Additionally we want to fail the build
    if the api.sh fails and thus we need the "-e" flag
    as well.
    
    Change-Id: I457026ef76b7f30f0c834c2210087e8b43f0b603

--- jenkins/gerrit-buck-build.sh
+++ jenkins/gerrit-buck-build.sh
@@ -8,7 +8,7 @@ buck build -v 3 api plugins:core release
 if [ -f tools/maven/api.sh ]
 then
   # From Gerrit 2.13 onwards
-  sh tools/maven/api.sh install
+  bash -e tools/maven/api.sh install
 else
   # Up to Gerrit 2.12
   buck build -v 3 api_install
