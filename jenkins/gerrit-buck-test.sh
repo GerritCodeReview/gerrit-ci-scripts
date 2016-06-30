482f28ddd651b51cbef9b0fa032015047f250ca9
Luca Milanesio <luca.milanesio@gmail.com>
2016-06-30 12:44:55 +0100

    Run tests during branch build
    At times unstable changes may be merged to their
    target branch, making the follow-up reviews unstable.
    
    Knowing that a target branch is red, helps understanding
    what and where is the problem.
    
    Change-Id: Ia82f77c9b9a04876fa67331e3df93490f00120b3

---
+++ jenkins/gerrit-buck-test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+MASTER_SHA1=$(git rev-parse origin/master)
+HEAD_SHA1=$(git rev-parse HEAD)
+
+rm -Rf plugins/*
+
+echo 'Test in default DB mode'
+echo '----------------------------------------------'
+buck test --no-results-cache --exclude flaky
+
+if [ "$HEAD_SHA1" != "$MASTER_SHA1" ]
+then
+  exit 0
+fi
+
+echo 'Test in Node DB mode'
+echo '----------------------------------------------'
+GERRIT_ENABLE_NOTEDB=TRUE buck test --no-results-cache --exclude flaky
+
+echo 'PolyGerrit UX tests'
+echo '----------------------------------------------'
+buck test --include web
+
+exit 0
