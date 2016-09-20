aa12e9cf39ef216e800ee1c53a23a9aab3069954
Luca Milanesio <luca.milanesio@gmail.com>
2016-09-20 08:05:09 -0400

    Build with Java 8 when required
    Change-Id: I5bce9bce612c9d8797493f8fd5c2ee4161b4a0c5

--- jenkins/gerrit-buck-build.sh
+++ jenkins/gerrit-buck-build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+if [ "$(grep source_level .buckconfig | awk '{print $3}')" == "8" ]
+  export JAVA_HOME=$JAVA8_HOME
+  export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
+fi
+
 buck build -v 3 api plugins:core release
 
 if [ -f tools/maven/api.sh ]
