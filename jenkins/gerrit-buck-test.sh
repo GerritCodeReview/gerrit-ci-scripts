7d393601688764124a639c8959b98cd9ffa493cc
Luca Milanesio <luca.milanesio@gmail.com>
2016-10-08 00:01:57 +0100

    Set Java version according to Buck source/target level
    Java 8 build has been introduced with Gerrit master
    whilst it is better for backward compatibility to keep
    on building with older releases.
    
    By extracting the Java level directoy from the .buckconfig
    file we are sure to always use the right version for the right
    branch.
    
    Change-Id: I988b4bab492b7a475b3586f1ccc9a48fabe5d63c

--- jenkins/gerrit-buck-test.sh
+++ jenkins/gerrit-buck-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-MASTER_SHA1=$(git rev-parse origin/master)
-HEAD_SHA1=$(git rev-parse HEAD)
+SOURCE_LEVEL=$(grep "source_level" .buckconfig || echo "source_level=7")
+. set-java.sh $(echo $SOURCE_LEVEL | cut -d '=' -f 2 | tr -d '[[:space:]]')
 
 rm -Rf plugins/*
 
@@ -9,7 +9,7 @@ echo 'Test in default DB mode'
 echo '----------------------------------------------'
 buck test --no-results-cache --exclude flaky
 
-if [ "$HEAD_SHA1" != "$MASTER_SHA1" ]
+if [ ! -d gerrit-server/src/main/java/com/google/gerrit/server/notedb ]
 then
   exit 0
 fi
@@ -17,6 +17,11 @@ fi
 echo 'Test in Node DB mode'
 echo '----------------------------------------------'
 GERRIT_ENABLE_NOTEDB=TRUE buck test --no-results-cache --exclude flaky
+
+if [ ! -d polygerrit-ui ]
+then
+  exit 0
+fi
 
 echo 'PolyGerrit UX tests'
 echo '----------------------------------------------'
