3e191e4c1b3f73ceb74383325eddabec1a745130
Luca Milanesio <luca.milanesio@gmail.com>
2016-10-07 16:29:28 +0100

    Build Gerrit master with Bazel
    Change-Id: Ie6e090aeda2f155613b1a54ed4e0643957ae82b8

---
+++ jenkins/gerrit-bazel-test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+echo 'Test in default DB mode'
+echo '----------------------------------------------'
+bazel test gerrit //...
+
+echo 'Test in Node DB mode'
+echo '----------------------------------------------'
+GERRIT_ENABLE_NOTEDB=TRUE bazel test //...
+
+exit 0
