2260e47f5282b335f7b97e98f113bfb547928aa2
Luca Milanesio <luca.milanesio@gmail.com>
2016-10-24 00:06:53 +0100

    Clone to gerrit sub-directory for build & test
    It seems that some parts of the tests are assuming
    that Gerrit is cloned into a directory named '*gerrit*'
    (PluginDaemonTest.java).
    
    Clone everything into gerrit to avoid the plugins tests
    to fail during build.
    
    Change-Id: I7d8d70fbf343906de396491fcf78d88d983fc014

--- jenkins/gerrit-buck-test.sh
+++ jenkins/gerrit-buck-test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
+cd gerrit
+
 SOURCE_LEVEL=$(grep "source_level" .buckconfig || echo "source_level=7")
 . set-java.sh $(echo $SOURCE_LEVEL | cut -d '=' -f 2 | tr -d '[[:space:]]')
-
-rm -Rf plugins/*
 
 echo 'Test in default DB mode'
 echo '----------------------------------------------'
