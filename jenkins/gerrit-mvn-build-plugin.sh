12fa6da5072e10737fc0e8988f46f8b4cbf8e231
Luca Milanesio <luca.milanesio@gmail.com>
2016-10-31 03:39:10 -0400

    Align plugins build to Gerrit master API
    When building the master version of a plugin, we need to make
    sure that the master version is using the latest version
    of Gerrit API and the correct JVM compatibility level.
    
    Change-Id: Id70a9cd7dcbdc8c824d2bb3ed66753d0561d65f7

--- jenkins/gerrit-mvn-build-plugin.sh
+++ jenkins/gerrit-mvn-build-plugin.sh
@@ -1,5 +1,16 @@
 #!/bin/bash -e
 
+SOURCE_LEVEL=$(grep "source_level" gerrit/.buckconfig || echo "source_level=7")
+. set-java.sh $(echo $SOURCE_LEVEL | cut -d '=' -f 2 | tr -d '[[:space:]]')
+
+if [ "{branch}" == "master" ]
+then
+  pushd gerrit
+  buck build api
+  ./tools/maven/api.sh install
+  popd
+fi
+
 find . -type d -name 'target' -delete
 mvn package
 
