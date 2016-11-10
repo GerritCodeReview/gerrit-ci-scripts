99fda05beaaab9a85a043ce417d3fb28a16374bd
David Pursehouse <dpursehouse@collab.net>
2016-11-10 11:35:43 +0900

    Fix typo in log message
    Change-Id: Ibbb47f2e1ac6849cb37b23543fd013578b975189

--- jenkins/gerrit-bazel-test.sh
+++ jenkins/gerrit-bazel-test.sh
@@ -4,7 +4,7 @@ echo 'Test in default DB mode'
 echo '----------------------------------------------'
 bazel test --test_output errors --flaky_test_attempts 3 --test_verbose_timeout_warnings --build_tests_only //...
 
-echo 'Test in Node DB mode'
+echo 'Test in Note DB mode'
 echo '----------------------------------------------'
 GERRIT_ENABLE_NOTEDB=TRUE bazel test --test_output errors --flaky_test_attempts 3 --test_verbose_timeout_warnings --build_tests_only //...
 
