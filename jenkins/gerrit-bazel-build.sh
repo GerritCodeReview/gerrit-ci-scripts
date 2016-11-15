af6813578f19e3a71a3b9691a20f56f9c61c8943
Luca Milanesio <luca.milanesio@gmail.com>
2016-11-15 11:29:41 -0800

    Unify Bazel and Buck verifiers jobs
    Change-Id: Ib0f5eb84453b79083c4fc6d0900cbb56539f6543

--- jenkins/gerrit-bazel-build.sh
+++ jenkins/gerrit-bazel-build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash -e
 
-bazel build gerrit-plugin-api:plugin-api_deploy.jar gerrit-extension-api:extension-api_deploy.jar 
-bazel build plugins:core
-bazel build release
+if [ -f "gerrit/BUILD" ]
+then
+  cd gerrit
 
-tools/maven/api.sh install bazel
+  bazel build gerrit-plugin-api:plugin-api_deploy.jar gerrit-extension-api:extension-api_deploy.jar
+  bazel build plugins:core
+  bazel build release
+
+  if [ -f "gerrit/BUCK" ]
+  then
+    tools/maven/api.sh install bazel
+  else
+    tools/maven/api.sh install
+  fi
+fi
