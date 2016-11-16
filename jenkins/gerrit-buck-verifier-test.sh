ef0d1882f0db8c9e624f343e30988201ca9890ae
David Ostrovsky <david@ostrovsky.org>
2016-11-15 17:00:38 -0800

    Set new environment option for NoteDb backend
    Change-Id: I0c204eabfa9f244813d3410bffbe25fe89cb437a

--- jenkins/gerrit-buck-verifier-test.sh
+++ jenkins/gerrit-buck-verifier-test.sh
@@ -15,7 +15,7 @@ then
 
   if [ "{mode}" == "notedb" ]
   then
-    export GERRIT_ENABLE_NOTEDB=TRUE
+    export GERRIT_NOTEDB=read-write
   fi
 
   if [ "{mode}" == "default" ] || [ "{mode}" == "notedb" ]
