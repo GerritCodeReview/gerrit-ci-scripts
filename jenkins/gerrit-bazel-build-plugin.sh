b97bf59510f5e98b86a08191e865d379812d1b76
Paladox <thomasmulhall410@yahoo.com>
2016-11-23 19:35:16 +0000

    Update to support bazel and buck tests (including plugins)
    Change-Id: I42c07fdcaf174b5d6bbc43f176d9f6e94f3eec74

---
+++ jenkins/gerrit-bazel-build-plugin.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+if [ -f 'gerrit/BUILD' ]
+then
+  git checkout -f gerrit/{branch}
+  rm -rf plugins/{name}
+  git read-tree -u --prefix=plugins/{name} origin/{branch}
+
+  TARGETS=$(echo "{targets}" | sed -e 's/{{name}}/{name}/g')
+
+  . set-java.sh 8
+
+  bazel build --spawn_strategy=standalone --genrule_strategy=standalone -v 3 $TARGETS
+
+  for JAR in $(bazel targets --show_output $TARGETS | awk '{{print $2}}')
+  do
+      PLUGIN_VERSION=$(git describe  --always origin/{branch})
+      echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
+      jar ufm $JAR MANIFEST.MF && rm MANIFEST.MF
+      DEST_JAR=bazel-genfiles/plugins/{name}/$(basename $JAR)
+      [ "$JAR" -ef "$DEST_JAR" ] || mv $JAR $DEST_JAR
+      echo "$PLUGIN_VERSION" > bazel-genfiles/plugins/{name}/$(basename $JAR-version)
+  done
+fi
