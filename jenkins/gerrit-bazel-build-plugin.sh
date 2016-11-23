9596dd28ffcfb163122db02dc314718a47b25b87
Paladox <thomasmulhall410@yahoo.com>
2016-11-23 19:35:16 +0000

    Update to support bazel and buck tests (including plugins)
    Change-Id: I42c07fdcaf174b5d6bbc43f176d9f6e94f3eec74

---
+++ jenkins/gerrit-bazel-build-plugin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+if [ -f 'gerrit/BUILD' ]
+then
+  git checkout -f gerrit/{branch}
+  rm -rf plugins/{name}
+  git read-tree -u --prefix=plugins/{name} origin/{branch}
+
+  TARGETS=$(echo "{targets}" | sed -e 's/{{name}}/{name}/g')
+
+  . set-java.sh 8
+
+  bazel build --spawn_strategy=standalone --genrule_strategy=standalone -v 3 $TARGETS
+fi
