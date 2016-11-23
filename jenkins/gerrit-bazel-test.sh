15241fff44a0ad247298dcec92aa076ec3a92747
Luca Milanesio <luca.milanesio@gmail.com>
2016-11-23 16:33:20 +0000

    Limit bazel tests parallelism to 1
    Some tests (e.g. ElasticSearch) seem to fail intermittently when
    executed in parallel with others. To prevent the build to become flaky
    it is safer to temporarily disable the parallelism, at least with Bazel.
    
    Change-Id: I6d5922175f7942e4d321054e10b038d6ef56b62d

--- jenkins/gerrit-bazel-test.sh
+++ jenkins/gerrit-bazel-test.sh
@@ -6,7 +6,8 @@ then
 
   export BAZEL_OPTS="--ignore_unsupported_sandboxing --test_output errors \
                      --test_summary detailed --flaky_test_attempts 3 \
-                     --test_verbose_timeout_warnings --build_tests_only" 
+                     --test_verbose_timeout_warnings --build_tests_only \
+                     --local_test_jobs 1"
 
   echo 'Test in default DB mode'
   echo '----------------------------------------------'
--- jenkins/gerrit-bazel-verifier-test.sh
+++ jenkins/gerrit-bazel-verifier-test.sh
@@ -19,7 +19,8 @@ then
     bazel test $GERRIT_NOTEDB \
                --ignore_unsupported_sandboxing --test_output errors \
                --test_summary detailed --flaky_test_attempts 3 \
-               --test_verbose_timeout_warnings --build_tests_only //...
+               --test_verbose_timeout_warnings --build_tests_only \
+               --local_test_jobs 1 //...
   fi
 
   if [ "$MODE" == "polygerrit" ]
