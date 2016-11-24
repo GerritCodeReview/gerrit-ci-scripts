814b2c65bad3e7211b7a5b33e32c79ab81f5b4e7
Luca Milanesio <luca.milanesio@gmail.com>
2016-11-24 16:27:53 +0000

    Add PolyGerrit execution on Bazel build
    Change-Id: Ib2cb861bc87b5c45d968c68ccaedd1e2e919ad3c

--- jenkins/gerrit-bazel-test.sh
+++ jenkins/gerrit-bazel-test.sh
@@ -16,6 +16,19 @@ then
   echo 'Test in Note DB mode'
   echo '----------------------------------------------'
   GERRIT_ENABLE_NOTEDB=TRUE bazel test $BAZEL_OPTS //...
+
+  echo 'Test PolyGerrit locally'
+  echo '----------------------------------------------'
+  sh ./polygerrit-ui/app/run_test.sh
+
+  if [ -z "$SAUCE_USERNAME" ] || [ -z "$SAUCE_ACCESS_KEY" ]
+  then
+    echo 'Not running on Sauce Labs because env vars are not set.'
+  else
+    echo 'Test PolyGerrit on Sauce Labs'
+    echo '----------------------------------------------'
+    WCT_ARGS='--plugin sauce' sh ./polygerrit-ui/app/run_test.sh
+  fi
 fi
 
 exit 0
