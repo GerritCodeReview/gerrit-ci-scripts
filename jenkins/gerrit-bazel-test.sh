ef7a2f0bd024618cd88f652f0f00af5a5f5bee5e
David Ostrovsky <david@ostrovsky.org>
2016-12-08 06:37:59 +0100

    Re-enable cookbook tests
    Now, that the cookbook tests bootstrap was re-implemented, the test
    execution time was reduced from 60 seconds to 8 seconds, we can
    activate the tests again during CI verification.
    
    Change-Id: I0352db489d0e912efd18d08212812cc545e71205

--- jenkins/gerrit-bazel-test.sh
+++ jenkins/gerrit-bazel-test.sh
@@ -9,7 +9,7 @@ then
                      --test_summary detailed --flaky_test_attempts 3 \
                      --test_verbose_timeout_warnings --build_tests_only \
                      --nocache_test_results \
-                     --test_tag_filters=-elastic,-cookbook-plugin,-flaky"
+                     --test_tag_filters=-elastic,-flaky"
 
   echo 'Test in default DB mode'
   echo '----------------------------------------------'
