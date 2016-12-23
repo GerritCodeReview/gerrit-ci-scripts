5fd0f11d7845bea59b87d9e451fd16b07026dc2d
Luca Milanesio <luca.milanesio@gmail.com>
2016-12-23 15:42:40 +0000

    Verify changes with Buck and Bazel in parallel
    Run all the verification jobs in parallel, combining:
    - tools (buck, build)
    - mode (standard, notedb, polygerrit)
    
    Then get all results and combine them together in a
    single verification feedback.
    
    This avoids keeping on spamming the change multiple times
    and giving +1 and -1 that are often misleading for the
    author of the change to interpret.
    
    Change-Id: Iebbaec91373a4088a9ef957ee42db33a5ad1dcbc

--- jenkins/gerrit-bazel-verifier-test.sh
+++ jenkins/gerrit-bazel-verifier-test.sh
@@ -7,24 +7,25 @@ cd gerrit
 echo "Test with mode=$MODE"
 echo '----------------------------------------------'
 
-if [ "$MODE" == "notedb" ]
-then
-  GERRIT_NOTEDB="--test_env=GERRIT_NOTEDB=READ_WRITE"
-fi
+export BAZEL_OPTS="--spawn_strategy=standalone --genrule_strategy=standalone \
+                 --test_output errors \
+                 --test_summary detailed --flaky_test_attempts 3 \
+                 --test_verbose_timeout_warnings --build_tests_only \
+                 --nocache_test_results \
+                 --test_tag_filters=-elastic,-flaky"
 
-if [ "$MODE" == "default" ] || [ "$MODE" == "notedb" ]
+if [[ "$MODE" == *"default"* ]]
 then
-  export BAZEL_OPTS="--spawn_strategy=standalone --genrule_strategy=standalone \
-                   --test_output errors \
-                   --test_summary detailed --flaky_test_attempts 3 \
-                   --test_verbose_timeout_warnings --build_tests_only \
-                   --nocache_test_results \
-                   --test_tag_filters=-elastic,-flaky"
-
   bazel test $GERRIT_NOTEDB $BAZEL_OPTS //...
 fi
 
-if [ "$MODE" == "polygerrit" ]
+if [[ "$MODE" == *"notedb"* ]]
+then
+  GERRIT_NOTEDB="--test_env=GERRIT_NOTEDB=READ_WRITE"
+  bazel test $GERRIT_NOTEDB $BAZEL_OPTS //...
+fi
+
+if [[ "$MODE" == *"polygerrit"* ]]
 then
   if [ -z "$DISPLAY" ]
   then
