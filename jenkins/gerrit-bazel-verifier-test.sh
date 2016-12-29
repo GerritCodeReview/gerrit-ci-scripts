706f13dbdf844c8946717f31a21fc6d6702c0913
Luca Milanesio <luca.milanesio@gmail.com>
2016-12-29 17:17:27 +0000

    Run slow tests serialised
    Gerrit slow tests are the one most likely to get stuck
    on deadlocks. Running them beforehand and serialized
    minimize the risks of a build getting stuck exactly
    at the end.
    
    Change-Id: Ic2751e14d3d10b5c9d473b4530b5403fc9fa5d69

--- jenkins/gerrit-bazel-verifier-test.sh
+++ jenkins/gerrit-bazel-verifier-test.sh
@@ -4,25 +4,34 @@ cd gerrit
 
 . set-java.sh 8
 
-echo "Test with mode=$MODE"
-echo '----------------------------------------------'
-
 export BAZEL_OPTS="--spawn_strategy=standalone --genrule_strategy=standalone \
                  --test_output errors \
                  --test_summary detailed --flaky_test_attempts 3 \
                  --test_verbose_timeout_warnings --build_tests_only \
-                 --nocache_test_results \
-                 --test_tag_filters=-elastic,-flaky"
+                 --nocache_test_results"
+
+function runTests {
+  echo ''
+  echo 'Running slow tests serialized ...'
+  echo ''
+  bazel test $1 $BAZEL_OPTS --test_tag_filters=+slow,-elastic,-flaky --local_test_jobs 1 //...
+  echo ''
+  echo 'Running fast tests in parallel ...'
+  bazel test $1 $BAZEL_OPTS --test_tag_filters=-slow,-elastic,-flaky --local_test_jobs 3 //...
+  echo ''  
+}
+
+echo "Test with mode=$MODE"
+echo '----------------------------------------------'
 
 if [[ "$MODE" == *"reviewdb"* ]]
 then
-  bazel test $BAZEL_OPTS //...
+  runTests
 fi
 
 if [[ "$MODE" == *"notedbReadWrite"* ]]
 then
-  GERRIT_NOTEDB="--test_env=GERRIT_NOTEDB=READ_WRITE"
-  bazel test $GERRIT_NOTEDB $BAZEL_OPTS //...
+  runTests '--test_env=GERRIT_NOTEDB=READ_WRITE'
 fi
 
 if [[ "$MODE" == *"polygerrit"* ]]
