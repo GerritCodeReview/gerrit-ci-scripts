4c0bf49efbc511a8a472d038f470de7515d37d0d
Luca Milanesio <luca.milanesio@gmail.com>
2016-12-30 08:39:55 +0000

    Disable Gerrit SSH-based test in verification jobs
    As discussed in [1] the current Gerrit integration tests on
    the SSH stack rely on JSch, which is actually flaky and quite unstable.
    
    We are investigating on alternative solutions to JSch for testing, but
    in the meantime we cannot afford blocking our builds every now and then.
    It is much better to avoid testing SSH for the time being in the
    verification jobs, keeping it in the main branches anyway.
    
    [1] https://groups.google.com/forum/#!topic/repo-discuss/p-8ZkoEj4p8
    
    Change-Id: I0d99b4587f507633a395fb6593fb9ba512351706

--- jenkins/gerrit-bazel-verifier-test.sh
+++ jenkins/gerrit-bazel-verifier-test.sh
@@ -12,7 +12,8 @@ export BAZEL_OPTS="--spawn_strategy=standalone --genrule_strategy=standalone \
                  --test_summary detailed --flaky_test_attempts 3 \
                  --test_verbose_timeout_warnings --build_tests_only \
                  --nocache_test_results \
-                 --test_tag_filters=-elastic,-flaky"
+                 --test_tag_filters=-elastic,-flaky" \
+                 --test_env=GERRIT_USE_SSH=NO
 
 if [[ "$MODE" == *"reviewdb"* ]]
 then
