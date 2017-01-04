f1517e85ed80087922955d716bd63f010fb32d27
Luca Milanesio <luca.milanesio@gmail.com>
2017-01-04 22:05:23 +0000

    Increase the build parallelism to 4
    So far the CI has been quite happy to manage up to 3 parallel
    changes validation. Let's try to see if we can pump up to 4
    which means > 16 concurrent builds.
    
    Change-Id: Iacabab406b52578dcdcbfdc4f1bb77dc41cd82b9

--- jenkins/gerrit-verifier-flow.groovy
+++ jenkins/gerrit-verifier-flow.groovy
@@ -32,7 +32,7 @@ class Globals {
   static int numRetryBuilds = 3
   static int myAccountId = 1022687
   static int waitForResultTimeout = 10000
-  static int maxBuilds = 3
+  static int maxBuilds = 4
   static String verifierJobName = "Gerrit-verifier-change"
 }
 
