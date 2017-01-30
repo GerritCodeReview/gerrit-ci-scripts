de29e13becf1cc8d56a540d03459bd66afb88a51
David Ostrovsky <david@ostrovsky.org>
2017-01-30 18:56:36 +0100

    Remove postbuild plugin artifact modification
    In Bazel tree build should correctly retrieve git version, so that
    postbuild step shouldn't be needed. If it doesn't work, then it
    should be fixed in Gerrit plugin build tool chain and not circumvent
    in the CI scripts.
    
    Change-Id: Ida34e31d2d317f21cbe0f9bc54e6dda7a0ff7049

--- jenkins/gerrit-bazel-build-plugin.sh
+++ jenkins/gerrit-bazel-build-plugin.sh
@@ -14,13 +14,3 @@ TARGETS=$(echo "{targets}" | sed -e 's/{{name}}/{name}/g')
 . set-java.sh 8
 
 bazel build --spawn_strategy=standalone --genrule_strategy=standalone $TARGETS
-
-for JAR in $(find bazel-genfiles/plugins/{name} -name {name}*.jar)
-do
-    PLUGIN_VERSION=$(git describe  --always origin/{branch})
-    echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
-    jar ufm $JAR MANIFEST.MF && rm MANIFEST.MF
-    DEST_JAR=bazel-genfiles/plugins/{name}/$(basename $JAR)
-    [ "$JAR" -ef "$DEST_JAR" ] || mv $JAR $DEST_JAR
-    echo "$PLUGIN_VERSION" > bazel-genfiles/plugins/{name}/$(basename $JAR-version)
-done
