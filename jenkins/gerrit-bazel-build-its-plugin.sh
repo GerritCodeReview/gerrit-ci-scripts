b5605852f53ed94c2f89b071e09f68dde34d3cd7
Paladox none <thomasmulhall410@yahoo.com>
2017-02-05 20:38:48 +0000

    Use find instead of ls in the its-plugin script
    This may fix this error https://gerrit-ci.gerritforge.com/job/plugin-its-storyboard-bazel-master/1/console
    
    Change-Id: I6d06278a35478ff1c5c2879226dac1040663265f

--- jenkins/gerrit-bazel-build-its-plugin.sh
+++ jenkins/gerrit-bazel-build-its-plugin.sh
@@ -18,7 +18,7 @@ fi
 bazel build --spawn_strategy=standalone --genrule_strategy=standalone plugins/its-{name}
 
 # Remove duplicate entries
-PLUGIN_JAR=$(ls $(pwd)/bazel-genfiles/plugins/its-{name}/its-{name}*.jar)
+PLUGIN_JAR=$(find bazel-genfiles/plugins/ -name its-{name}*.jar)
 mkdir jar-out && pushd jar-out
 jar xf $PLUGIN_JAR && jar cmf META-INF/MANIFEST.MF $PLUGIN_JAR .
 popd
