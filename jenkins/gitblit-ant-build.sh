9700871447b9aeeb4e8d3319b6589833f9e004ac
Luca Milanesio <luca.milanesio@gmail.com>
2017-02-10 14:02:14 +0000

    Gitblit plugin build with Bazel
    Change-Id: I0dfbd4002e4e439cd5f7d10beadb81d0bad4b878

--- jenkins/gitblit-ant-build.sh
+++ jenkins/gitblit-ant-build.sh
@@ -1,7 +1,25 @@
 #!/bin/bash -ex
 
 git checkout origin/{branch}
-GITBLIT_TAG=$(expr "`grep com.gitblit lib/BUCK`" : '[^:]*:[^:]*:\([0-9\.]*\)')
+if [ -f lib/BUILD ]
+then
+  GITBLIT_LINE=$(grep com.gitblit external_plugin_deps.bzl)
+  if [ expr "$GITBLIT_LINE" : '[^:]*:[^:]*:[0-9\.]*-SNAPSHOT' ]
+  then
+    GITBLIT_TAG=ref/heads/master
+  else
+    GITBLIT_TAG=ref/tags/v$(expr "$GITBLIT_LINE" : '[^:]*:[^:]*:\([0-9\.]*\)')
+  fi
+else
+  GITBLIT_TAG=ref/tags/v$(expr "`grep com.gitblit lib/BUCK`" : '[^:]*:[^:]*:\([0-9\.]*\)')
+fi
 
 git fetch gitblit refs/tags/v$GITBLIT_TAG && git checkout FETCH_HEAD
+
+# Apply PR#1168 for Lucene compatibility with Gerrit master
+if [ "$GITBLIT_TAG" == "refs/heads/master" ]
+then
+  git fetch gitblit refs/pull/1168/head && git cherry-pick FETCH_HEAD
+fi
+
 ant -DresourceFolderPrefix=static installMaven
