2ad8b3c6a7b48fe62b90db5a12a948ac09ce5a13
Luca Milanesio <luca.milanesio@gmail.com>
2017-02-10 14:02:14 +0000

    Gitblit plugin build with Bazel
    Change-Id: I0dfbd4002e4e439cd5f7d10beadb81d0bad4b878

--- jenkins/gitblit-ant-build.sh
+++ jenkins/gitblit-ant-build.sh
@@ -1,7 +1,27 @@
 #!/bin/bash -ex
 
 git checkout origin/{branch}
-GITBLIT_TAG=$(expr "`grep com.gitblit lib/BUCK`" : '[^:]*:[^:]*:\([0-9\.]*\)')
+if [ -f BUILD ]
+then
+  GITBLIT_LINE=$(grep com.gitblit external_plugin_deps.bzl)
+  if expr "$GITBLIT_LINE" : '.*[0-9\.]*-SNAPSHOT.*'
+  then
+    GITBLIT_REF=refs/heads/master
+  else
+    GITBLIT_REF=refs/tags/v$(expr "$GITBLIT_LINE" : '[^:]*:[^:]*:\([0-9\.]*\)')
+  fi
+else
+  GITBLIT_REF=refs/tags/v$(expr "`grep com.gitblit lib/BUCK`" : '[^:]*:[^:]*:\([0-9\.]*\)')
+fi
 
-git fetch gitblit refs/tags/v$GITBLIT_TAG && git checkout FETCH_HEAD
+git fetch gitblit $GITBLIT_REF && git checkout FETCH_HEAD
+
+# Apply PR#1168 for Lucene compatibility with Gerrit master
+if [ "$GITBLIT_REF" == "refs/heads/master" ]
+then
+  git config user.name "Gerrit CI"
+  git config user.email "jenkins@gerritforge.com"
+  git fetch gitblit refs/pull/1168/head && git merge --no-edit FETCH_HEAD
+fi
+
 ant -DresourceFolderPrefix=static installMaven
