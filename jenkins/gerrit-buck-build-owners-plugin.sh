a01007c3bee7bf92e55a887a3fcc937d8065f720
Luca Milanesio <luca.milanesio@gmail.com>
2017-03-21 23:22:27 +0000

    Build owners on stable-2.13 with Buck
    Change-Id: Iad3b872e2f0eda316f0ee7d58ae95e47625aaace

---
+++ jenkins/gerrit-buck-build-owners-plugin.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+# NOTE: All Gerrit plugins currently depend on, at a minimum
+# the bucklets repo, and most plugins don't build outside the
+# Gerrit tree, even with bucklets.  As a result, we need to
+# graft the plugin onto the Gerrit repo.  We do this by checking
+# out the proper version of Gerrit, removing the plugin if it
+# exists, then use git read-tree to put the plugin we're
+# building in place.
+git checkout -f gerrit/{branch}
+git read-tree -u --prefix=plugins/owners-plugin origin/{branch}
+cd plugins && ln -s owners-plugin/owners* . && cd ..
+cat plugins/owners-plugin/.buckconfig >> .buckconfig
+
+TARGETS=$(echo "{targets}" | sed -e 's/{{owners}}/owners/g')
+
+. set-java.sh 8
+
+buck build -v 3 $TARGETS
+
+for JAR in $(find buck-out/gen/plugins/ -regex '.*/owners\(-autoassign\)*\.jar')
+do
+    PLUGIN_VERSION=$(git describe  --always origin/{branch})
+    echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
+    jar ufm $JAR MANIFEST.MF && rm MANIFEST.MF
+    DEST_JAR=buck-out/gen/plugins/{name}/$(basename $JAR)
+    [ "$JAR" -ef "$DEST_JAR" ] || mv $JAR $DEST_JAR
+    echo "$PLUGIN_VERSION" > buck-out/gen/plugins/{name}/$(basename $JAR-version)
+done
