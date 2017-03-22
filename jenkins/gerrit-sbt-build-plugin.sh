652ba14c5fb5a08a38e8381a2be2d46880a1f76e
Luca Milanesio <luca.milanesio@gmail.com>
2017-03-22 22:22:45 +0000

    Add support for building Scala-based plugins
    Introduce support for the first Gerrit plugin written in Scala
    and for potentially other future plugins.
    
    Change-Id: I32bcc0640854dfea01617e8408708b985d863dc1

---
+++ jenkins/gerrit-sbt-build-plugin.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+if [ "{branch}" == "master" ]
+then
+  git read-tree -u --prefix=gerrit gerrit/{branch}
+  . set-java.sh 8
+
+  pushd gerrit
+  bazel build api
+  ./tools/maven/api.sh install
+  popd
+fi
+
+sbt assembly
+
+# Extract version information
+PLUGIN_JARS=$(find . -name '{repo}*jar')
+for jar in $PLUGIN_JARS
+do
+  PLUGIN_VERSION=$(git describe  --always origin/{branch})
+  echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
+  jar ufm $jar MANIFEST.MF && rm MANIFEST.MF
+
+  echo "$PLUGIN_VERSION" > $jar-version
+done
