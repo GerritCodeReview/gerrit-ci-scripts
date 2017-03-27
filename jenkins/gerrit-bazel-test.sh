307a5b3ac13f8e83e04e00bb37aff1e31f06dd5d
Luca Milanesio <luca.milanesio@gmail.com>
2017-03-27 22:41:19 +0100

    Set Java Heap size allocation for Bazel tests
    Recently tests have been randomly failing because of JVM
    running out of heap space. Increase the allocation to
    see if it is more memory what Gerrit needs or just a leak
    inside the code-base.
    
    Change-Id: I95e49537199ab76b04075c1b87c32db4fa7b2e48

--- jenkins/gerrit-bazel-test.sh
+++ jenkins/gerrit-bazel-test.sh
@@ -2,13 +2,14 @@
 
 cd gerrit
 
-export BAZEL_OPTS="--spawn_strategy=standalone --genrule_strategy=standalone \
-                   --test_output errors \
-                   --test_summary detailed --flaky_test_attempts 3 \
-                   --test_verbose_timeout_warnings --build_tests_only \
-                   --nocache_test_results \
-                   --test_timeout 3600 \
-                   --test_tag_filters=-elastic,-flaky"
+export BAZEL_OPTS='--spawn_strategy=standalone --genrule_strategy=standalone
+                   --test_output errors
+                   --test_summary detailed --flaky_test_attempts 3
+                   --test_verbose_timeout_warnings --build_tests_only
+                   --nocache_test_results
+                   --test_timeout 3600
+                   --jvmopt=-Xmx16g
+                   --test_tag_filters=-elastic,-flaky'
 
 echo 'Test in default DB mode'
 echo '----------------------------------------------'
--- jenkins/gerrit-bazel-verifier-test.sh
+++ jenkins/gerrit-bazel-verifier-test.sh
@@ -7,13 +7,14 @@ cd gerrit
 echo "Test with mode=$MODE"
 echo '----------------------------------------------'
 
-export BAZEL_OPTS="--spawn_strategy=standalone --genrule_strategy=standalone \
-                 --test_output errors \
-                 --test_summary detailed --flaky_test_attempts 3 \
-                 --test_verbose_timeout_warnings --build_tests_only \
-                 --nocache_test_results \
-                 --test_timeout 3600 \
-                 --test_tag_filters=-elastic,-flaky"
+export BAZEL_OPTS='--spawn_strategy=standalone --genrule_strategy=standalone
+                 --test_output errors
+                 --test_summary detailed --flaky_test_attempts 3
+                 --test_verbose_timeout_warnings --build_tests_only
+                 --nocache_test_results
+                 --test_timeout 3600
+                 --jvmopt=-Xmx16g
+                 --test_tag_filters=-elastic,-flaky'
 
 if [[ "$MODE" == *"reviewdb"* ]]
 then
