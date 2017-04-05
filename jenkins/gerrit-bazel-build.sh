2626df6597917524bc1b42470271ccbee585a82c
Luca Milanesio <luca.milanesio@gmail.com>
2017-04-05 23:01:45 +0100

    Deploy SNAPSHOTs to Maven
    One of the common problems of developing a plugin
    for Gerrit is to retrieve API dependencies from Maven
    for a non-release version of Gerrit.
    
    Push from now on all the successful SNAPSHOT builds
    to Maven, so that plugin developers are not required anymore
    to clone and build Gerrit API locally.
    
    Change-Id: Ie253f3e1e11b22850c96efbc8482e9f5d2b64872

--- jenkins/gerrit-bazel-build.sh
+++ jenkins/gerrit-bazel-build.sh
@@ -9,5 +9,15 @@ bazel build $BAZEL_OPTS \
       gerrit-plugin-api:plugin-api_deploy.jar \
       gerrit-extension-api:extension-api_deploy.jar
 
+if [ -f ~/.m2/settings.xml ]
+then
+  if grep -q sonatype-nexus-staging ~/.m2/settings.xml
+  then
+    echo "Deploying artifacts to Maven ..."
+    /tools/maven/api.sh deploy
+    echo "DONE"
+  fi
+fi
+
 bazel build $BAZEL_OPTS plugins:core
 bazel build $BAZEL_OPTS release
