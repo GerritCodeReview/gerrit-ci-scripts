48bb74a2c7da86bc3e1571a63b01fc89cc4a35dc
Luca Milanesio <luca.milanesio@gmail.com>
2017-04-26 21:17:19 +0200

    Decouple plugin's and Gerrit's branches
    Allow a plugin branch to be built against multiple versions of Gerrit
    without requiring the creation of extra stable branches
    
    Change-Id: I0ace1ffb7fd7a67d20f7e78cb68cafa94177f1e7

---
+++ jenkins/gerrit-bazel-build-plugin-branch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+. set-java.sh 8
+
+echo "Building plugin {name}/{branch} with Gerrit/{gerrit-branch}"
+
+git remote add gerrit https://gerrit.googlesource.com/gerrit
+git checkout -f gerrit/{gerrit-branch}
+git read-tree -u --prefix=plugins/{name} origin/{branch}
+
+if [ -f plugins/{name}/external_plugin_deps.bzl ]
+then
+  cp -f plugins/{name}/external_plugin_deps.bzl plugins/
+fi
+
+TARGETS=$(echo "{targets}" | sed -e 's/{{name}}/{name}/g')
+
+bazel build --spawn_strategy=standalone --genrule_strategy=standalone $TARGETS
+
+for JAR in $(find bazel-genfiles/plugins/{name} -name {name}*.jar)
+do
+    PLUGIN_VERSION=$(git describe  --always origin/{branch})
+    echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
+    jar ufm $JAR MANIFEST.MF && rm MANIFEST.MF
+    DEST_JAR=bazel-genfiles/plugins/{name}/$(basename $JAR)
+    [ "$JAR" -ef "$DEST_JAR" ] || mv $JAR $DEST_JAR
+    echo "$PLUGIN_VERSION" > bazel-genfiles/plugins/{name}/$(basename $JAR-version)
+done
