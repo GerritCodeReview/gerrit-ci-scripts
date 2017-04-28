98adb9be6ce7e50a483646221b39419adcad6ba9
Luca Milanesio <luca.milanesio@gmail.com>
2017-04-28 21:13:03 +0000

    Do not deploy to Maven on stable branches
    Change-Id: I553f9adacb9d5640046460f0faa1c42fc7773706

--- jenkins/gerrit-deploy-api.sh
+++ jenkins/gerrit-deploy-api.sh
@@ -1,11 +1,21 @@
-#!/bin/bash -e
+#!/bin/bash -xe
+
+cd gerrit 
+
+VERSION=$(([ -f VERSION ] && grep VERSION VERSION) || ([ -f version.bzl ] && \
+          grep VERSION version.bzl) || echo "")
+if ! expr "$VERSION" : '.*SNAPSHOT.*'
+then
+  echo "Release build: not publishing the API to Snapshot Repository"
+  exit 0
+fi
 
 if [ -f ~/.m2/settings.xml ]
 then
   if grep -q sonatype-nexus-staging ~/.m2/settings.xml
   then
     echo "Deploying artifacts to Maven ..."
-    cd gerrit && ./tools/maven/api.sh deploy
+    ./tools/maven/api.sh deploy
     echo "DONE"
   fi
 fi
