4b609df02f76c563015480f903cd80a2c49e6e09
Luca Milanesio <luca.milanesio@gmail.com>
2017-05-01 00:59:25 +0100

    Cross-build its-bugzilla for Gerrit 2.14
    Change-Id: I840821e8a3cfd2845b0d843ec9d51038faaee8ae

---
+++ jenkins/gerrit-bazel-build-its-plugin-branch.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+
+git remote add gerrit https://gerrit.googlesource.com/gerrit
+git fetch gerrit
+git checkout gerrit/{gerrit-branch}
+rm -rf plugins/its-{name}
+rm -rf plugins/its-base
+git read-tree -u --prefix=plugins/its-{name} origin/{branch}
+git read-tree -u --prefix=plugins/its-base base/{branch}
+
+rm -Rf bazel-genfiles
+
+if [ -f plugins/its-{name}/external_plugin_deps.bzl ]
+then
+  cp -f plugins/its-{name}/external_plugin_deps.bzl plugins/
+fi
+
+. set-java.sh 8
+
+bazel build --spawn_strategy=standalone --genrule_strategy=standalone plugins/its-{name}
+
+# Remove duplicate entries
+PLUGIN_JAR=$(ls $(pwd)/bazel-genfiles/plugins/its-{name}/its-{name}*.jar)
+mkdir jar-out && pushd jar-out
+jar xf $PLUGIN_JAR && rm -f $PLUGIN_JAR && jar cmf META-INF/MANIFEST.MF $PLUGIN_JAR .
+popd
+
+# Extract version information
+PLUGIN_VERSION=$(git describe --always origin/{branch})
+echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
+jar ufm $PLUGIN_JAR MANIFEST.MF && rm MANIFEST.MF
+
+echo "$PLUGIN_VERSION" > $PLUGIN_JAR-version
