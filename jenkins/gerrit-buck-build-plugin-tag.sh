8fab45026632dcb0370404db1ca72f805d91cbcb
Luca Milanesio <luca.milanesio@gmail.com>
2017-05-01 01:31:16 +0100

    Cross-build oauth for Gerrit 2.1[0-4]
    Change-Id: I10aec8f27b5de51dca66ce704c3c1d9ac7bda46e

---
+++ jenkins/gerrit-buck-build-plugin-tag.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+# NOTE: All Gerrit plugins currently depend on, at a minimum
+# the bucklets repo, and most plugins don't build outside the
+# Gerrit tree, even with bucklets.  As a result, we need to
+# graft the plugin onto the Gerrit repo.  We do this by checking
+# out the proper version of Gerrit, removing the plugin if it
+# exists, then use git read-tree to put the plugin we're
+# building in place.
+git remote add gerrit https://gerrit.googlesource.com/gerrit
+git fetch gerrit
+git checkout -f gerrit/{gerrit-branch}
+rm -rf plugins/{name}
+git read-tree -u --prefix=plugins/{name} origin/{tag}
+
+TARGETS=$(echo "{targets}" | sed -e 's/{{name}}/{name}/g')
+
+SOURCE_LEVEL=$(grep "source_level" .buckconfig || echo "source_level=7")
+. set-java.sh $(echo $SOURCE_LEVEL | cut -d '=' -f 2 | tr -d '[[:space:]]')
+
+buck build -v 3 $TARGETS
+
+for JAR in $(buck targets --show_output $TARGETS | awk '{{print $2}}')
+do
+    PLUGIN_VERSION={tag}
+    echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
+    jar ufm $JAR MANIFEST.MF && rm MANIFEST.MF
+    DEST_JAR=buck-out/gen/plugins/{name}/$(basename $JAR)
+    [ "$JAR" -ef "$DEST_JAR" ] || mv $JAR $DEST_JAR
+    echo "$PLUGIN_VERSION" > buck-out/gen/plugins/{name}/$(basename $JAR-version)
+done
