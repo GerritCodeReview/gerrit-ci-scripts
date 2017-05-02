92fc36c98b6ef895a23633d177963d208c1f6b29
Dave Borowitz <dborowitz@google.com>
2017-05-02 11:19:13 -0400

    Replace GERRIT_NOTEDB=PRIMARY with FUSED
    googlesource.com is running in FUSED mode now, and PRIMARY is a few
    migration steps ago.
    
    Just remove PRIMARY from the buck verifier, since no version of Gerrit
    that builds with buck supports FUSED.
    
    Change-Id: Ia1479ca0dfea19f66fd5517927a439a11d114cf9

--- jenkins/gerrit-bazel-verifier-test.sh
+++ jenkins/gerrit-bazel-verifier-test.sh
@@ -20,15 +20,15 @@ then
   bazel test $BAZEL_OPTS //...
 fi
 
-if [[ "$MODE" == *"notedbPrimary"* ]]
-then
-  GERRIT_NOTEDB="--test_env=GERRIT_NOTEDB=PRIMARY"
-  bazel test $GERRIT_NOTEDB $BAZEL_OPTS //...
-fi
-
 if [[ "$MODE" == *"disableChangeReviewDb"* ]]
 then
   GERRIT_NOTEDB="--test_env=GERRIT_NOTEDB=DISABLE_CHANGE_REVIEW_DB"
+  bazel test $GERRIT_NOTEDB $BAZEL_OPTS //...
+fi
+
+if [[ "$MODE" == *"fused"* ]]
+then
+  GERRIT_NOTEDB="--test_env=GERRIT_NOTEDB=FUSED"
   bazel test $GERRIT_NOTEDB $BAZEL_OPTS //...
 fi
 
