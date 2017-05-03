1420fadb7df8286c2499892960549f8113044418
Luca Milanesio <luca.milanesio@gmail.com>
2017-05-03 23:45:13 +0100

    Don't validate NoteDb on stable-2.14
    NoteDb isn't a supported feature on stable-2.14
    and the fused mode is not even available as an option.
    
    Testing NoteDb on stable-2.14 would just product
    broken builds.
    
    Change-Id: I4fa6629427d809e0f410af101a8adf450f0f35ac

--- jenkins/gerrit-verifier-change.groovy
+++ jenkins/gerrit-verifier-change.groovy
@@ -220,10 +220,12 @@ def buildChange(change) {
   println "Building Change " + changeUrl
   build.setDescription("""<a href='$changeUrl' target='_blank'>Change #$changeNum</a>""")
 
-  if(branch == "master" || branch == "stable-2.14") {
+  if(branch == "master") {
     modes += "disableChangeReviewDb"
     modes += "fused"
+  }
 
+  if(branch == "master" || branch == "stable-2.14") {
     def changedFiles = getChangedFiles(changeNum, sha1)
     def polygerritFiles = changedFiles.findAll { it.startsWith("polygerrit-ui") }
 
