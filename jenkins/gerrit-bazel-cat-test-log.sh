ecacc78786ca04d6c5cc770368deb17992c2b6c9
Paladox none <thomasmulhall410@yahoo.com>
2017-05-24 13:18:54 +0000

    If log file dosent't exist don't fail the polygerrit test
    This was causing problems on stable-2.14 and possibly master.
    
    Change-Id: If1f58dc2307832db5422c930662b172f35814a9f

--- jenkins/gerrit-bazel-cat-test-log.sh
+++ jenkins/gerrit-bazel-cat-test-log.sh
@@ -7,7 +7,7 @@ then
    echo "See test log below"
    echo "=============================="
    echo ""
-   cat $(ls ~/.cache/bazel/_bazel_jenkins/*/execroot/gerrit/bazel-out/local-fastbuild/testlogs/polygerrit-ui/app/wct_test/test.log)
+   cat $(ls ~/.cache/bazel/_bazel_jenkins/*/execroot/gerrit/bazel-out/local-fastbuild/testlogs/polygerrit-ui/app/wct_test/test.log 2>/dev/null)
    exit -1
 fi
 
