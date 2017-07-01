82ce05225831c065bb819cfbde0013ed7383d3ad
David Ostrovsky <david@ostrovsky.org>
2017-07-01 22:42:47 +0200

    Implement google-java-format check and vote on code-style label
    Change-Id: Ia9f2ab0f36c70a644f27ba3305be566a445f0773

--- jenkins/gerrit-verifier-change.groovy
+++ jenkins/gerrit-verifier-change.groovy
@@ -76,7 +76,7 @@ def gerritPost(url, jsonPayload) {
   return 0
 }
 
-def gerritReview(change, sha1, verified) {
+def gerritLabelVerify(change, sha1, verified) {
   if(verified == 0) {
     return;
   }
@@ -92,13 +92,6 @@ def gerritReview(change, sha1, verified) {
     "${resTicks[it.res]} ${it.type} : ${it.res}\n    (${it.url})"
   } .join('\n')
 
-  def addReviewerExit = gerritPost("a/changes/" + changeNum + "/reviewers", '{ "reviewer" : "' +
-                                   Globals.gerritReviewer + "\" , ${Globals.addReviewerTag} }")
-  if(addReviewerExit != 0) {
-    println "**** ERROR: cannot add myself as reviewer of change " + changeNum + " *****"
-    return addReviewerExit
-  }
-
   def addVerifiedExit = gerritLabel(changeNum, sha1, 'Verified', verified, msgBody)
   if(addVerifiedExit == 0) {
     println "----------------------------------------------------------------------------"
@@ -106,11 +99,43 @@ def gerritReview(change, sha1, verified) {
     println "----------------------------------------------------------------------------"
   }
 
+  return addVerifiedExit
+}
+
+def findCodestyleFilesInLog() {
+  def codestyleFiles = []
+  def needsFormatting = false
+  def codestyleLogReader = Globals.buildsList["codestyle"].getLogReader()
+  codestyleLogReader.eachLine {
+    needsFormatting = needsFormatting || (it ==~ /.*Need Formatting.*/)
+    if(needsFormatting && it ==~ /\[.*\]) {
+      codestyleFiles += it.substring(1,it.length-1)
+    }
+  }
+
+  return codestyleFiles
+}
+
+def gerritLabelCodestyle(change, sha1, cs, files) {
+  if(cs == 0) {
+    return;
+  }
+
+  def changeNum = change._number
+  def msgBody = "The following files need formatting:\n" + files.join('\n')
+
+  def addReviewerExit = gerritPost("a/changes/" + changeNum + "/reviewers", '{ "reviewer" : "' +
+                                   Globals.gerritReviewer + "\" , ${Globals.addReviewerTag} }")
+  if(addReviewerExit != 0) {
+    println "**** ERROR: cannot add myself as reviewer of change " + changeNum + " *****"
+    return addReviewerExit
+  }
+
   if(Globals.codeStyleBranches.contains(change.branch)) {
-    def addCodeStyleExit = gerritLabel(changeNum, sha1, 'Code-Style', verified)
-    if(addVerifiedExit == 0) {
+    def addCodeStyleExit = gerritLabel(changeNum, sha1, 'Code-Style', cs, msgBody)
+    if(addCodeStyleExit == 0) {
       println "----------------------------------------------------------------------------"
-      println "Gerrit Review: Code-Style=" + verified + " to change " + changeNum + "/" + sha1
+      println "Gerrit Review: Code-Style=" + cs + " to change " + changeNum + "/" + sha1
       println "----------------------------------------------------------------------------"
     }
   }
@@ -174,8 +199,20 @@ def getChangedFiles(changeNum, sha1) {
   filesJson.keySet().findAll { it != "/COMMIT_MSG" }
 }
 
-def buildsForMode(refspec,sha1,changeUrl,mode,tools,targetBranch,retryTimes) {
+def buildsForMode(refspec,sha1,changeUrl,mode,tools,targetBranch,retryTimes,codestyle) {
     def builds = []
+    if(codestyle) {
+      builds += {
+        Globals.buildsList.put("codestyle", build("Gerrit-codestyle",
+                               REFSPEC: refspec, BRANCH: sha1, CHANGE_URL: changeUrl, MODE: mode,
+                               TARGET_BRANCH: targetBranch))
+                     println "Builds status:"
+                     Globals.buildsList.each {
+                       n, v -> println "  $n : ${v.getResult()}\n    (${v.getBuildUrl() + "console"})"
+                     }
+      }
+    }
+
     for (tool in tools) {
       def buildName = "Gerrit-verifier-$tool"
       def key = "$tool/$mode"
@@ -259,9 +296,14 @@ def buildChange(change) {
 
   def builds = []
   println "Running validation jobs using $tools builds for $modes ..."
-  modes.collect { buildsForMode(refspec,sha1,changeUrl,it,tools,branch,1) }.each { builds += it }
+  modes.collect {
+    buildsForMode(refspec,sha1,changeUrl,it,tools,branch,1,Globals.codeStyleBranches.contains(branch))
+  }.each { builds += it }
 
   def buildsWithResults = parallelBuilds(builds)
+  def codestyleResult = buildsWithResults.find{ it[0] == "codestyle" }
+  def resCodeStyle = getVerified(1, codestyleResult[1])
+  gerritLabelCodestyle(change, sha1, resCodeStyle, findCodestyleFilesInLog())
 
   flaky = flakyBuilds(buildsWithResults)
   if(flaky.size > 0) {
@@ -279,11 +321,13 @@ def buildChange(change) {
     buildsWithResults = parallelBuilds(retryBuilds)
   }
 
-  def res = buildsWithResults.inject(1) { acc, buildResult -> getVerified(acc, buildResult[1]) }
+  def resVerify = buildsWithResults.findAll{ it != codestyleResult }.inject(1) { acc, buildResult -> getVerified(acc, buildResult[1]) }
 
-  gerritReview(change, sha1, res)
+  def resAll = getVerified(resVerify, codestyleResult[1])
 
-  switch(res) {
+  gerritLabelVerify(change, sha1, resVerify, resCodeStyle)
+
+  switch(resAll) {
     case 0: build.state.result = ABORTED
             break
     case 1: build.state.result = SUCCESS
