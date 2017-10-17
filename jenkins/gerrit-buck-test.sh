87dbcb9589b0953730feacf2a41f5a75b77082c0
Luca Milanesio <luca.milanesio@gmail.com>
2017-10-17 08:20:27 +0100

    Do not test NoteDB and PolyGerrit on buck
    For Buck builds (<= 2.13) it does not make sense
    anymore to keep on testing NoteDb and PolyGerrit UX
    because those branches are not actively developed
    anymore.
    
    Change-Id: I18b39d248af01492f4a6345a13683cb387ca99de

--- jenkins/gerrit-buck-test.sh
+++ jenkins/gerrit-buck-test.sh
@@ -15,21 +15,6 @@ then
   then
     exit 0
   fi
-
-  echo 'Test in Node DB mode'
-  echo '----------------------------------------------'
-  export GERRIT_NOTEDB=READ_WRITE
-  buck test --no-results-cache --exclude flaky
-
-  if [ ! -d polygerrit-ui ]
-  then
-    exit 0
-  fi
-
-  echo 'PolyGerrit UX tests (on Java8)'
-  echo '----------------------------------------------'
-  . set-java.sh 8
-  buck test --include web
 fi
 
 exit 0
