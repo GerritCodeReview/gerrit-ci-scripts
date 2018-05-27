5cc3706930d82b25dfe29d4366b7b92196e91d4d
David Pursehouse <dpursehouse@collab.net>
2018-05-27 15:28:53 +0900

    Exclude docker tests on CI
    Change-Id: Idb3470102893157241690fc6b7507bfaec5de0f8

--- jenkins/gerrit-bazel-test.sh
+++ jenkins/gerrit-bazel-test.sh
@@ -7,7 +7,7 @@ export BAZEL_OPTS="--spawn_strategy=standalone --genrule_strategy=standalone \
                    --test_summary detailed --flaky_test_attempts 3 \
                    --test_verbose_timeout_warnings --build_tests_only \
                    --test_timeout 3600 \
-                   --test_tag_filters=-flaky"
+                   --test_tag_filters=-flaky,-docker"
 
 echo 'Test in ReviewDb mode'
 echo '----------------------------------------------'
