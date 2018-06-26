067ed5a35a129edfd546f2d3185bf6a7e6b5d0e7
Thomas Draebing <thomas.draebing@sap.com>
2018-06-26 10:51:32 +0200

    Simplify selection of build tools in gerrit-verifier-change.groovy
    In gerrit-verifier-change.groovy the script decided whether Buck or
    Bazel is used to build Gerrit. As it was implemented the build tools
    were collected in a list, which would in theory allow multiple build
    tools, but as it was implemented always only one build tool was chosen.
    
    This change simplifies the script to assume always just one build tool
    and not using lists.
    
    Change-Id: I1b6d612b9e8d5566af086e95c2152cf4e742d2df

--- jenkins/gerrit-verifier-change.groovy
+++ jenkins/gerrit-verifier-change.groovy
@@ -204,7 +204,7 @@ def getLabelValue(acc, res) {
   }
 }
 
-def prepareBuildsForMode(mode,tools,retryTimes,codestyle) {
+def prepareBuildsForMode(mode,tool,retryTimes,codestyle) {
     def builds = []
     if(codestyle) {
       builds += {
@@ -219,21 +219,20 @@ def prepareBuildsForMode(mode,tools,retryTimes,codestyle) {
       }
     }
 
-    for (tool in tools) {
-      def buildName = "Gerrit-verifier-$tool"
-      def key = "$tool/$mode"
-      builds += {
-                   retry (retryTimes) {
-                     Config.buildsList.put(key,
-                       build(buildName, REFSPEC: Change.ref, BRANCH: Change.sha1,
-                             CHANGE_URL: Change.changeUrl, MODE: mode, TARGET_BRANCH: Change.branch))
-                     println "Builds status:"
-                     Config.buildsList.each {
-                       n, v -> println "  $n : ${v.getResult()}\n    (${v.getBuildUrl() + "consoleText"})"
-                     }
-                   }
-                }
-    }
+    def buildName = "Gerrit-verifier-$tool"
+    def key = "$tool/$mode"
+    builds += {
+                  retry (retryTimes) {
+                    Config.buildsList.put(key,
+                      build(buildName, REFSPEC: Change.ref, BRANCH: Change.sha1,
+                            CHANGE_URL: Change.changeUrl, MODE: mode, TARGET_BRANCH: Change.branch))
+                    println "Builds status:"
+                    Config.buildsList.each {
+                      n, v -> println "  $n : ${v.getResult()}\n    (${v.getBuildUrl() + "consoleText"})"
+                    }
+                  }
+              }
+
     return builds
 }
 
@@ -264,14 +263,14 @@ def initializeGit(cwd){
     runSh(cwd, 'git merge --no-commit --no-edit --no-ff FETCH_HEAD')
 }
 
-def collectBuildTools(cwd){
-    def tools = []
+def collectBuildTool(cwd){
+    def tool = ""
     if(new java.io.File("$cwd/BUCK").exists()) {
-        tools += ["buck"]
+        tool = "buck"
     } else if(new java.io.File("$cwd/BUILD").exists()) {
-        tools += ["bazel"]
+        tool = "bazel"
     }
-    return tools
+    return tool
 }
 
 def collectBuildModes(){
@@ -301,16 +300,16 @@ def collectBuildModes(){
 def buildChange() {
   def cwd = getWorkspace()
   initializeGit(cwd)
-  def tools = collectBuildTools(cwd)
+  def tool = collectBuildTool(cwd)
   def modes = collectBuildModes()
 
   println "Building Change " + Change.changeUrl
   build.setDescription("""<a href='$Change.changeUrl' target='_blank'>Change #$Change.changeNum</a>""")
 
   def builds = []
-  println "Running validation jobs using $tools builds for $modes ..."
+  println "Running validation jobs using $tool builds for $modes ..."
   modes.collect {
-    prepareBuildsForMode(it,tools,1,Config.codeStyleBranches.contains(Change.branch))
+    prepareBuildsForMode(it,tool,1,Config.codeStyleBranches.contains(Change.branch))
   }.each { builds += it }
 
   def buildsWithResults = getResultsOfBuildsInParallel(builds)
@@ -373,7 +372,7 @@ def retryFlakyBuilds(flaky){
       def tool = it[0]
       def mode = it[1]
       Config.buildsList.remove(it)
-      retryBuilds += prepareBuildsForMode(mode,[tool],3,false)
+      retryBuilds += prepareBuildsForMode(mode,tool,3,false)
     }
     return getResultsOfBuildsInParallel(retryBuilds)
 }
