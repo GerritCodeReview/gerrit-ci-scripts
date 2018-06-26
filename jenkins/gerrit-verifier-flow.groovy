aaf86a38ec17140df75e729b098519c280ce0fb7
Thomas Draebing <thomas.draebing@sap.com>
2018-06-26 11:24:30 +0200

    Refactor groovy-scripts to use a 4-space indent style
    The groovy-scripts used in Jenkins jobs used mostly 2-space and
    sometimes 4-space indentation.
    This change applies a 4-space indentation consistently in the scripts.
    4-space indentation was chosen, since the embedded editor in the
    Jenkins-UI uses this style.
    
    Change-Id: I8c042f3df6c373755782381596f87ec24e56d60f

--- jenkins/gerrit-verifier-flow.groovy
+++ jenkins/gerrit-verifier-flow.groovy
@@ -20,32 +20,32 @@ import groovy.json.*
 import java.text.*
 
 String.metaClass.encodeURL = {
-  java.net.URLEncoder.encode(delegate)
+    java.net.URLEncoder.encode(delegate)
 }
 
 class Globals {
-  static String gerrit = "https://gerrit-review.googlesource.com/"
-  static String jenkins = "https://gerrit-ci.gerritforge.com/"
-  static String gerritReviewer = "GerritForge CI <gerritforge@gmail.com>"
-  static long curlTimeout = 10000
-  static SimpleDateFormat tsFormat = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss.S Z")
-  static int maxChanges = 100
-  static int numRetryBuilds = 3
-  static int myAccountId = 1022687
-  static int waitForResultTimeout = 10000
-  static int maxBuilds = 16
-  static String verifierJobName = "Gerrit-verifier-change"
+    static String gerrit = "https://gerrit-review.googlesource.com/"
+    static String jenkins = "https://gerrit-ci.gerritforge.com/"
+    static String gerritReviewer = "GerritForge CI <gerritforge@gmail.com>"
+    static long curlTimeout = 10000
+    static SimpleDateFormat tsFormat = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss.S Z")
+    static int maxChanges = 100
+    static int numRetryBuilds = 3
+    static int myAccountId = 1022687
+    static int waitForResultTimeout = 10000
+    static int maxBuilds = 16
+    static String verifierJobName = "Gerrit-verifier-change"
 }
 
 def lastBuild = build.getPreviousSuccessfulBuild()
 
 def lastBuildStartTimeMillis = lastBuild == null ?
-  (System.currentTimeMillis() - 1800000) : lastBuild.getStartTimeInMillis()
+    (System.currentTimeMillis() - 1800000) : lastBuild.getStartTimeInMillis()
 def sinceMillis = lastBuildStartTimeMillis - (24 * 3600 * 1000)
 def since = Globals.tsFormat.format(new Date(sinceMillis))
 
 if(lastBuild != null) {
-  println "Last successful build was " + lastBuild.toString()
+    println "Last successful build was " + lastBuild.toString()
 }
 
 println ""
@@ -53,68 +53,70 @@ println "Querying Gerrit for last modified changes since ${since} ..."
 
 def gerritQuery = "status:open project:gerrit since:\"" + since + "\""
 
-queryUrl = new URL(Globals.gerrit + "changes/?pp=0&o=CURRENT_REVISION&o=DETAILED_ACCOUNTS&o=DETAILED_LABELS&n=" + Globals.maxChanges + "&q=" +
-                      gerritQuery.encodeURL())
+queryUrl = new URL(Globals.gerrit +
+    "changes/?pp=0&o=CURRENT_REVISION&o=DETAILED_ACCOUNTS&o=DETAILED_LABELS&n=" +
+    Globals.maxChanges + "&q=" + gerritQuery.encodeURL())
 
 def changes = queryUrl.getText().substring(5)
 def jsonSlurper = new JsonSlurper()
 def changesJson = jsonSlurper.parseText(changes)
 
 def getBuildRunsInProgress() {
-  def verifierChangeJob = Hudson.instance.getJob(Globals.verifierJobName)
-  def verifierRuns = verifierChangeJob.builds
-  def pendingBuilds = verifierRuns.findAll { it.building }
-  return pendingBuilds
+    def verifierChangeJob = Hudson.instance.getJob(Globals.verifierJobName)
+    def verifierRuns = verifierChangeJob.builds
+    def pendingBuilds = verifierRuns.findAll { it.building }
+    return pendingBuilds
 }
 
 def changeOfBuildRun(run) {
-  def params = run.allActions.findAll { it in hudson.model.ParametersAction }
-  return params.collect { it.getParameter("CHANGE_ID").value }
+    def params = run.allActions.findAll { it in hudson.model.ParametersAction }
+    return params.collect { it.getParameter("CHANGE_ID").value }
 }
 
 def changeUrl(change) {
-  "${Globals.gerrit}${change}"
+    "${Globals.gerrit}${change}"
 }
 
 def acceptedChanges = changesJson.findAll {
-  change ->
-  sha1 = change.current_revision
-  if(sha1 == null) {
-      println "[WARNING] Skipping change ${changeUrl(change._number)} because it does not have any current revision or patch-set"
-      return false
-  }
+    change ->
+        sha1 = change.current_revision
+        if(sha1 == null) {
+            println "[WARNING] Skipping change ${changeUrl(change._number)} because it does not have any current revision or patch-set"
+            return false
+        }
 
-  if(!change.mergeable) {
-      println "[WARNING] Skipping change ${changeUrl(change._number)} because has merge conflicts"
-      return false
-  }
+        if(!change.mergeable) {
+            println "[WARNING] Skipping change ${changeUrl(change._number)} because has merge conflicts"
+            return false
+        }
 
-  if(change.hashtags.contains("skipci")) {
-      println "[WARNING] Skipping change ${changeUrl(change._number)} because it is tagged with #skipci"
-      return false
-  }
+        if(change.hashtags.contains("skipci")) {
+            println "[WARNING] Skipping change ${changeUrl(change._number)} because it is tagged with #skipci"
+            return false
+        }
 
-  def canBeVerified = change.labels.Verified
-  if(canBeVerified != null) {
-    def verified = canBeVerified.all
-    if(verified == null) {
-      true
-    } else {
-      def myVerifications = verified.findAll {
-        verification -> verification._account_id == Globals.myAccountId && verification.value != 0
-      }
-      return myVerifications.empty
-    }
-  }
+        def canBeVerified = change.labels.Verified
+        if(canBeVerified != null) {
+            def verified = canBeVerified.all
+            if(verified == null) {
+            true
+            } else {
+            def myVerifications = verified.findAll {
+                verification ->
+                    verification._account_id == Globals.myAccountId && verification.value != 0
+            }
+            return myVerifications.empty
+            }
+        }
 }
 
 def inProgress = getBuildRunsInProgress()
 if(!inProgress.empty) {
-  println ""
-  println "Changes currently in progress: "
-  inProgress.each {
-    b -> println("Change ${changeUrl(changeOfBuildRun(b)[0])}: ${Globals.jenkins}${b.url}")
-  }
+    println ""
+    println "Changes currently in progress: "
+    inProgress.each {
+        b -> println("Change ${changeUrl(changeOfBuildRun(b)[0])}: ${Globals.jenkins}${b.url}")
+    }
 }
 
 def inProgressChangesNums = inProgress.collect { changeOfBuildRun(it) }.flatten()
@@ -126,22 +128,23 @@ def buildsBandwith = Globals.maxBuilds - inProgressChangesNums.size
 println ""
 println "Gerrit has " + filteredChanges.size() + " change(s) since " + since + " $filteredChanges"
 if(buildsBandwith <= 0) {
-  println "... but there is NO bandwidth for further builds yet"
+    println "... but there is NO bandwidth for further builds yet"
 }
 else {
-  if(filteredChanges.size() > buildsBandwith) {
-    println "... but I've got bandwidth for only ${buildsBandwith} of them at the moment"
-  }
+    if(filteredChanges.size() > buildsBandwith) {
+        println "... but I've got bandwidth for only ${buildsBandwith} of them at the moment"
+    }
 }
 println "================================================================================"
 
 if(buildsBandwith > 0) {
-  def changesTodo = filteredChanges.reverse().take(buildsBandwith)
+    def changesTodo = filteredChanges.reverse().take(buildsBandwith)
 
-  changesTodo.each {
-    println "Building change: ${changeUrl(it)} ..."
-  }
+    changesTodo.each {
+        println "Building change: ${changeUrl(it)} ..."
+    }
 
-  def builds = changesTodo.collect { change -> { -> build(Globals.verifierJobName, CHANGE_ID: change) } }
-  parallel(builds)
+    def builds = changesTodo.collect { change ->
+        { -> build(Globals.verifierJobName, CHANGE_ID: change) } }
+    parallel(builds)
 }
