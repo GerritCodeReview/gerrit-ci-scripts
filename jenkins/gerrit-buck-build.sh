8254b059701625c8fe61590212c60fec020a7398
Luca Milanesio <luca.milanesio@gmail.com>
2018-07-10 03:29:54 -0400

    Cleanup Buck build scripts
    Buck builds up to Gerrit v2.13 do not officially
    support neither NoteDb nor PolyGerrit.
    Get rid of all conditional statements related to them
    and simplify the build scripts.
    
    Change-Id: Ief1510391e1fd1c5d0b3bc425127cad7c4b447b9

--- jenkins/gerrit-buck-build.sh
+++ jenkins/gerrit-buck-build.sh
@@ -1,21 +1,18 @@
 #!/bin/bash -e
 
-if [ -f 'gerrit/BUCK' ]
+cd gerrit
+
+SOURCE_LEVEL=$(grep "source_level" .buckconfig || echo "source_level=7")
+. set-java.sh $(echo $SOURCE_LEVEL | cut -d '=' -f 2 | tr -d '[[:space:]]')
+
+buck build -v 3 api plugins:core release
+
+if [ -f tools/maven/api.sh ]
 then
-  cd gerrit
-
-  SOURCE_LEVEL=$(grep "source_level" .buckconfig || echo "source_level=7")
-  . set-java.sh $(echo $SOURCE_LEVEL | cut -d '=' -f 2 | tr -d '[[:space:]]')
-
-  buck build -v 3 api plugins:core release
-
-  if [ -f tools/maven/api.sh ]
-  then
-    # From Gerrit 2.13 onwards
-    tools/maven/api.sh install buck
-  else
-    # Up to Gerrit 2.12
-    buck build -v 3 api_install
-  fi
-  mv $(find buck-out -name '*.war') buck-out/gen/gerrit.war
+  # From Gerrit 2.13 onwards
+  tools/maven/api.sh install buck
+else
+  # Up to Gerrit 2.12
+  buck build -v 3 api_install
 fi
+mv $(find buck-out -name '*.war') buck-out/gen/gerrit.war
