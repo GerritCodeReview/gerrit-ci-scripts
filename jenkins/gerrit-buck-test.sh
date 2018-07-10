ef5c56e8dbd8f505d5cf8d86f648dcfc679a06a4
Luca Milanesio <luca.milanesio@gmail.com>
2018-07-10 03:29:54 -0400

    Cleanup Buck build scripts
    Buck builds up to Gerrit v2.13 do not officially
    support neither NoteDb nor PolyGerrit.
    Get rid of all conditional statements related to them
    and simplify the build scripts.
    
    Change-Id: Ief1510391e1fd1c5d0b3bc425127cad7c4b447b9

--- jenkins/gerrit-buck-test.sh
+++ jenkins/gerrit-buck-test.sh
@@ -1,20 +1,10 @@
 #!/bin/bash -e
 
-if [ -f "gerrit/BUCK" ]
-then
-  cd gerrit
+cd gerrit
 
-  SOURCE_LEVEL=$(grep "source_level" .buckconfig || echo "source_level=7")
-  . set-java.sh $(echo $SOURCE_LEVEL | cut -d '=' -f 2 | tr -d '[[:space:]]')
+SOURCE_LEVEL=$(grep "source_level" .buckconfig || echo "source_level=7")
+. set-java.sh $(echo $SOURCE_LEVEL | cut -d '=' -f 2 | tr -d '[[:space:]]')
 
-  echo 'Test in default DB mode'
-  echo '----------------------------------------------'
-  buck test --no-results-cache --exclude flaky
-
-  if [ ! -d gerrit-server/src/main/java/com/google/gerrit/server/notedb ]
-  then
-    exit 0
-  fi
-fi
-
-exit 0
+echo 'Test in default DB mode'
+echo '----------------------------------------------'
+buck test --no-results-cache --exclude flaky
--- jenkins/gerrit-buck-verifier-test.sh
+++ jenkins/gerrit-buck-verifier-test.sh
@@ -1,42 +1,16 @@
 #!/bin/bash -e
 set +x
 
-if [ -f "gerrit/BUCK" ]
+cd gerrit
+
+SOURCE_LEVEL=$(grep "source_level" .buckconfig || echo "source_level=7")
+. set-java.sh $(echo $SOURCE_LEVEL | cut -d '=' -f 2 | tr -d '[[:space:]]')
+
+echo "Test with mode=$MODE"
+echo '----------------------------------------------'
+
+if [[ "$MODE" == *"reviewdb"* ]]
 then
-  cd gerrit
-
-  SOURCE_LEVEL=$(grep "source_level" .buckconfig || echo "source_level=7")
-  . set-java.sh $(echo $SOURCE_LEVEL | cut -d '=' -f 2 | tr -d '[[:space:]]')
-
-  echo "Test with mode=$MODE"
-  echo '----------------------------------------------'
-
-  if [[ "$MODE" == *"reviewdb"* ]]
-  then
-    buck test --no-results-cache --exclude flaky
-  fi
-
-  if [[ "$MODE" == *"notedb"* ]]
-  then
-    export GERRIT_NOTEDB=ON
-    buck test --no-results-cache --exclude flaky
-  fi
-
-  if [[ "$MODE" == *"polygerrit"* ]]
-  then
-    if [ -z "$DISPLAY" ]
-    then
-      echo 'Not running local tests because env var "DISPLAY" is not set.'
-    else
-      echo 'Running local tests...'
-      buck test --include web
-    fi
-    if [ -z "$SAUCE_USERNAME" ] || [ -z "$SAUCE_ACCESS_KEY" ]
-    then
-      echo 'Not running on Sauce Labs because env vars are not set.'
-    else
-      echo 'Running tests on Sauce Labs...'
-      WCT_ARGS='--plugin sauce' buck test --no-results-cache --include web
-    fi
-  fi
+  buck test --no-results-cache --exclude flaky
 fi
+
