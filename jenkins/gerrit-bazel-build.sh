a86195c07395119c6aa5fdfca1e47b611893ef78
David Pursehouse <dpursehouse@collab.net>
2018-07-19 11:40:14 +0900

    Enable ExpectedException check in bazel build
    Enabling this check with severity "error" will prevent changes that
    inadvertently misuse the ExpectedException. See [1] for details.
    
    [1] http://errorprone.info/bugpattern/ExpectedExceptionChecker
    
    Bug: Issue 9442
    Change-Id: Ice1d2dd090d33013999e9374de6642f8a9971bb0

--- jenkins/gerrit-bazel-build.sh
+++ jenkins/gerrit-bazel-build.sh
@@ -3,7 +3,7 @@
 cd gerrit
 . set-java.sh 8
 
-export BAZEL_OPTS="--spawn_strategy=standalone --genrule_strategy=standalone"
+export BAZEL_OPTS="--javacopt=-Xep:ExpectedExceptionChecker:ERROR --spawn_strategy=standalone --genrule_strategy=standalone"
 
 bazel build $BAZEL_OPTS plugins:core release api
 tools/eclipse/project.py
