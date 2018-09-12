24302c3f3dbce5e2b74e15b9cdc5b7289329bff3
David Pursehouse <dpursehouse@collab.net>
2018-09-12 10:40:07 +0900

    gerrit-bazel-build: Use error_prone_warnings_toolchain_bazel_0.16
    Change-Id: I6ae091633ddcb0811e0a00320e56501fb79a638d

--- jenkins/gerrit-bazel-build.sh
+++ jenkins/gerrit-bazel-build.sh
@@ -3,7 +3,7 @@
 cd gerrit
 . set-java.sh 8
 
-export BAZEL_OPTS="--spawn_strategy=standalone --genrule_strategy=standalone"
+export BAZEL_OPTS="--spawn_strategy=standalone --genrule_strategy=standalone --java_toolchain //tools:error_prone_warnings_toolchain_bazel_0.16"
 
 bazel build $BAZEL_OPTS //...
 tools/eclipse/project.py
