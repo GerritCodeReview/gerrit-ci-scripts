265470a89e658e7721ce2e25f4f6c53631c693b4
Luca Milanesio <luca.milanesio@gmail.com>
2018-12-07 22:40:35 +0000

    Docker images for making Gerrit releases
    Provide a standard way of producing Gerrit releases by extending
    the Jenkins CI slave docker image and adding the extra packages
    for being able to generate a release war and jars:
    * GnuPG2
    * GCloud CLI
    
    Include the 'gerrit-release.sh' automation script to perform
    all the standard actions for releasing Gerrit up to the maven
    upload to the Sonatype staging repository.
    
    Change-Id: I9a476f2fd94849889ee89e1e30ad76c0f1f52cce

---
+++ jenkins-docker/slave-release/gerrit-release.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -e
+
+if [ "$1" == "" ] || [ "$2" == "" ]
+then
+  echo "Gerrit Code Review - release automation script"
+  echo "----------------------------------------------"
+  echo "Use: $0 <branch> <version>"
+  echo ""
+  echo "Where: branch  Gerrit branch name where the release must but cut"
+  echo "       version Gerrit semantic release number"
+  echo ""
+  echo "Example: $0 stable-2.16 2.16.1"
+  exit 1
+fi
+
+export branch=$1
+export version=$2
+
+if [ -d gerrit ]
+then
+  rm -Rf gerrit
+fi
+
+echo "Cloning and build Gerrit Code Review on branch $branch ..."
+git clone https://gerrit.googlesource.com/gerrit
+pushd gerrit
+
+source set-java.sh 8
+
+git checkout $branch
+git fetch && git reset --hard origin/$branch
+git submodule update --init
+
+git clean -fdx
+./tools/version.py $version
+git commit -a -m "Set version to $version"
+git tag -f -s -m "v$version" "v$version"
+git submodule foreach git tag -f -s -m "v$version" "v$version"
+
+bazel build release Documentation:searchfree
+./tools/maven/api.sh install
+
+echo -n "Checking Gerrit version ... "
+
+warVersion=$(java -jar bazel-bin/release.war --version)
+
+if ! [ "$warVersion" == "gerrit version $version" ]
+then
+  echo "Version build is $warVersion while was expecting $version"
+  exit 2
+fi
+
+echo "OK"
+
+echo "Checking Gerrit plugins version ... "
+java -jar bazel-bin/release.war init --list-plugins
+
+echo "Publishing Gerrit WAR and APIs to Maven Central ..."
+export VERBOSE=1
+./tools/maven/api.sh war_deploy
+./tools/maven/api.sh deploy
+
+echo "Release completed"
+echo "Download the artifacts from SonaType staging repository at https://oss.sonatype.org"
+echo "logging in using your credentials"
