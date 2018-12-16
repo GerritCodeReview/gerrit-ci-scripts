e0272840d0f2069eab009783e95db2cbff14bbfd
David Ostrovsky <david@ostrovsky.org>
2018-12-17 00:41:02 +0100

    Discontinue Java 7 and Debian Jessie version
    End of life of Java 7 was April 2015. The only reason we use unsupported
    Debian Jessie version is to support Java 7.
    
    The way forward is:
    
    1. Announce, that old Gerrit versions: 2.11 and 2.12 have eol for
    community support
    2. Announce, that 2.13 branch is now built with Java 8
    
    Test Plan:
    
    1. Build all dockers images
    2. Start slave-bazel docker image:
    
    docker run --privileged -it gerritforge/gerrit-ci-slave-bazel:debian-stretch bash
    
    3. su - jenkins
    4. clone gerrit:
    
    git clone --recurse-submodules https://gerrit.googlesource.com/gerrit
    
    5. cd gerrit
    6. Build with bazel:
    
    bazel build :release
    
    7. Switch to stable-2.13 branch:
    
    git checkout stable-2.13
    
    8. Build with buck:
    
    buck build :release
    
    Change-Id: I427f5291a2f5683a447e806f7681c291ded7aa62

--- jenkins-docker/slave-buck/set-buck.sh
+++ jenkins-docker/slave-buck/set-buck.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-if [ "$1" == "" ]
-then
-  echo "Set current Buck/Java version level"
-  echo ""
-  echo "Use: $0 <7|8>"
-  exit 1
-fi
-
-export PATH=/opt/buck-java$1/bin:$PATH
+export PATH=/opt/buck-java/bin:$PATH
 
 echo "Buck set to: $(which buck)"
