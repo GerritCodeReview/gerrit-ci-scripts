806235010d0e3d4e25c27420c8944b013eb5b41f
David Ostrovsky <david.ostrovsky@gmail.com>
2018-12-27 10:04:31 +0000

    Buck: Don't use --no-results-cache on older Buck versions
    Older Buck versions don't support that option.
    
    Change-Id: Iff174d2fbeed7263ceeef8dc52c76b6d60d0db85

--- jenkins/gerrit-buck-test.sh
+++ jenkins/gerrit-buck-test.sh
@@ -5,6 +5,13 @@ cd gerrit
 SOURCE_LEVEL=$(grep "source_level" .buckconfig || echo "source_level=7")
 . set-java.sh $(echo $SOURCE_LEVEL | cut -d '=' -f 2 | tr -d '[[:space:]]')
 
+BUCK_OPTIONS=""
+
+if ([ "$TARGET_BRANCH" == "stable-2.13" ] || [ "$TARGET_BRANCH" == "stable-2.12" ] || [ "$TARGET_BRANCH" == "stable-2.11" ] || [ "$TARGET_BRANCH" == "stable-2.10" ])
+then
+  BUCK_OPTIONS="--no-results-cache"
+fi
+
 echo 'Test in default DB mode'
 echo '----------------------------------------------'
-buck test --no-results-cache --exclude flaky
+buck test $BUCK_OPTIONS --exclude flaky
