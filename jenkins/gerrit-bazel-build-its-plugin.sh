29031365193badd297982b139da3b33109ccb2c4
Luca Milanesio <luca.milanesio@gmail.com>
2019-01-26 23:44:36 +0000

    Run tests on its-jira plugin
    Change-Id: Ia3c558c71961c7cce692b281b788bb0b48a4d791

--- jenkins/gerrit-bazel-build-its-plugin.sh
+++ jenkins/gerrit-bazel-build-its-plugin.sh
@@ -13,19 +13,25 @@ then
   cp -f plugins/its-{name}/external_plugin_deps.bzl plugins/
 fi
 
+TARGETS=$(echo "{targets}" | sed -e 's/its-{{name}}/its-{name}/g')
+TEST_TARGET=$(grep -2 junit_tests plugins/its-{name}/BUILD | grep -o 'name = "[^"]*"' | cut -d '"' -f 2)
+
 . set-java.sh 8
 
-bazel build --spawn_strategy=standalone --genrule_strategy=standalone plugins/its-{name}
+bazel build --spawn_strategy=standalone --genrule_strategy=standalone $TARGETS
 
-# Remove duplicate entries
-PLUGIN_JAR=$(ls $(pwd)/bazel-genfiles/plugins/its-{name}/its-{name}*.jar)
-mkdir jar-out && pushd jar-out
-jar xf $PLUGIN_JAR && rm -f $PLUGIN_JAR && jar cmf META-INF/MANIFEST.MF $PLUGIN_JAR .
-popd
+if [ "$TEST_TARGET" != "" ]
+then
+    bazel test plugins/its-{name}:$TEST_TARGET
+fi
 
-# Extract version information
-PLUGIN_VERSION=$(git describe --always origin/{branch})
-echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
-jar ufm $PLUGIN_JAR MANIFEST.MF && rm MANIFEST.MF
+for JAR in $(find bazel-genfiles/plugins/its-{name} -name its-{name}*.jar)
+do
+    PLUGIN_VERSION=$(git describe  --always origin/{branch})
+    echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
+    jar ufm $JAR MANIFEST.MF && rm MANIFEST.MF
+    DEST_JAR=bazel-genfiles/plugins/its-{name}/$(basename $JAR)
+    [ "$JAR" -ef "$DEST_JAR" ] || mv $JAR $DEST_JAR
+    echo "$PLUGIN_VERSION" > bazel-genfiles/plugins/its-{name}/$(basename $JAR-version)
+done
 
-echo "$PLUGIN_VERSION" > $PLUGIN_JAR-version
