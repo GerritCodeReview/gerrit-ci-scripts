f3e5c27879bc829288e6f8ad27c3172f827f44b9
Thomas Draebing <thomas.draebing@sap.com>
2019-03-18 17:48:23 +0100

    Switch from GitHub OAuth to certificate-based authentication
    The current production setup uses the github-oauth plugin for
    authentication of Administrators. This setup could not easily be used
    for test instances, which let the running test setups exposed.
    
    This change implements certificate-based authentication instead. The new
    authentication method works as follows:
    
    - Jenkins is not exposing any port outside of the docker-network that is
      created by docker-compose. Thus, only connections to Jenkins from
      inside the same docker network will be possible (currently only from
      the Nginx container).
    - A dockerized Nginx is used as a reverse proxy for ingress traffic.
    - The reverse proxy forwards HTTP-traffic (port 80) to HTTPS (port
      443)
    - The reverse proxy allows HTTPS-traffic over port 443 access as
      anonymous user.
    - The reverse proxy accepts HTTPS-traffic over port 443 using the
      gerrit-ci-secure.gerritforge.com hostname.
      SSL-verification is activated for this server. Only if the certificate
      signed by the CA mounted into the Nginx-container for this purpose,
      the request is forwarded to Jenkins. Nginx extracts the CN-field of
      the certificates DN and forwards it as the `X-Forwarded-User`-header.
    - The reverse-proxy-auth-plugin of Jenkins extracts the username from
      the header and sets it in Jenkins.
    - Matrix-based authorization is used to configure permissions as before.
      Anonymous users have read-access and authenticated users are admins.
    - To allow jenkins-job-builder to work, Nginx allows traffic to port
      8081 and forwards those requests to Jenkins with the username JJB,
      which possesses all permissions required by jenkins-job-builder. The
      port 8081 is not exposed to the outside of the docker-network. Thus,
      currently only a connection from the Jenkins- and the Nginx-container
      itself are possible to this port.
    - Docker-compose in combination with GNU make is used to easily start up
      the reverse proxy and Jenkins.
    
    Change-Id: I8b2efd9c0c0414a98fb08c5b005f4d32d07d9ff4

--- jenkins-docker/master/config.sh
+++ jenkins-docker/master/config.sh
@@ -14,13 +14,8 @@ usermod -g $DOCKER_GID jenkins
 xsltproc \
   --stringparam use-security $USE_SECURITY \
   --stringparam docker-url $DOCKER_HOST \
-  --stringparam oauth-client-id $OAUTH_ID \
-  --stringparam oauth-client-secret $OAUTH_SECRET \
   $JENKINS_REF/edit-config.xslt $CONFIG > /tmp/config.xml.new
 mv /tmp/config.xml.new $CONFIG
-
-sed -i -e "s/user=.*/user=$JENKINS_API_USER/" /etc/jenkins_jobs/jenkins_jobs.ini
-sed -i -e "s/password=.*/password=$JENKINS_API_PASSWORD/" /etc/jenkins_jobs/jenkins_jobs.ini
 
 cp -R $JENKINS_REF/.ssh ~jenkins/.
 chown -R jenkins:dockergroup ~jenkins
