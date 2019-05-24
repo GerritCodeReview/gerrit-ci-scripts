743986c9d0a5a0639169910e6480c93116a3a6c7
David Ostrovsky <david@ostrovsky.org>
2019-05-24 23:02:13 +0200

    Remove error prone toolchain activation
    This toolchain is already enabled in gerrit tree in .bazelrc per default
    so that we don't need to activate it explicitly in the CI anymore.
    
    Change-Id: I5304079540fb8fcd4a46b85c297fe963a0e8268a

--- jenkins/gerrit-bazel-build.sh
+++ jenkins/gerrit-bazel-build.sh
@@ -3,7 +3,7 @@
 cd gerrit
 . set-java.sh 8
 
-export BAZEL_OPTS="--spawn_strategy=standalone --genrule_strategy=standalone --java_toolchain //tools:error_prone_warnings_toolchain"
+export BAZEL_OPTS="--spawn_strategy=standalone --genrule_strategy=standalone"
 
 bazelisk version
 bazelisk build $BAZEL_OPTS plugins:core release api
