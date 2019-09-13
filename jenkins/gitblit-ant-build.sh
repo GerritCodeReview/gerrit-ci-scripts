17d7fd71db513708e069e5274152090cb7d75d80
Thomas Draebing <thomas.draebing@sap.com>
2019-09-13 10:27:49 +0200

    Remove buck-references in Gitblit jobs
    Change-Id: Ib5e7d9243e3de6e46027e26d45e7c4104d3ecd1a

--- jenkins/gitblit-ant-build.sh
+++ jenkins/gitblit-ant-build.sh
@@ -1,17 +1,13 @@
 #!/bin/bash -ex
 
 git checkout origin/{branch}
-if [ -f BUILD ]
+
+GITBLIT_LINE=$(grep com.gitblit external_plugin_deps.bzl)
+if expr "$GITBLIT_LINE" : '.*[0-9\.]*-SNAPSHOT.*'
 then
-  GITBLIT_LINE=$(grep com.gitblit external_plugin_deps.bzl)
-  if expr "$GITBLIT_LINE" : '.*[0-9\.]*-SNAPSHOT.*'
-  then
-    GITBLIT_REF=refs/heads/master
-  else
-    GITBLIT_REF=refs/tags/v$(expr "$GITBLIT_LINE" : '[^:]*:[^:]*:\([0-9\.]*\)')
-  fi
+  GITBLIT_REF=refs/heads/master
 else
-  GITBLIT_REF=refs/tags/v$(expr "`grep com.gitblit lib/BUCK`" : '[^:]*:[^:]*:\([0-9\.]*\)')
+  GITBLIT_REF=refs/tags/v$(expr "$GITBLIT_LINE" : '[^:]*:[^:]*:\([0-9\.]*\)')
 fi
 
 git fetch gitblit $GITBLIT_REF && git checkout FETCH_HEAD
