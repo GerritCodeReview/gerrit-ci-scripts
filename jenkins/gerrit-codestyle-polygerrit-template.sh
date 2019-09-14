d49214869b6e31065c0480b4905591f612be60bb
David Ostrovsky <david@ostrovsky.org>
2019-09-14 09:56:09 +0200

    Use Debian buster as docker image base
    This change also removes java8 environment file, because Debian buster
    doesn't have Java 8 any more so that Java 11 is used per default. This
    doesn't change target Java bytecode in build tool chain, though: Bazel
    is still targeting Java 8 per default.
    
    Change-Id: Ifda364809f0597f6dc782a0c5da886670d1b4326

--- jenkins/gerrit-codestyle-polygerrit-template.sh
+++ jenkins/gerrit-codestyle-polygerrit-template.sh
@@ -8,7 +8,6 @@ if ([ "$TARGET_BRANCH" == "master" ] || \
     && git show --diff-filter=AM --name-only --pretty="" HEAD | grep polygerrit-ui
 then
   echo 'Running PolyGerrit template test...'
-  . set-java.sh 8
   java -fullversion
   bazelisk version
   bazelisk test //polygerrit-ui/app:all --test_tag_filters=template --test_output errors
