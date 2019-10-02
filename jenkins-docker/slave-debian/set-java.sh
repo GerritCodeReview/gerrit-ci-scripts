2c55a77d4555bb8786804ad44e1d1daa2a2fdf34
Thomas Draebing <thomas.draebing@sap.com>
2019-10-02 14:14:21 +0200

    Verify Gerrit with Java11 toolchain on master branch
    The job building Gerrit with the Java11 toolchain was executed on
    a cron-schedule, but did not verify changes.
    
    This change adds the java toolchain used as a parameter to the jobs
    building Gerrit, allowing to set up jobs for each branch using each
    a configured Java toolchain.
    
    Change-Id: Ib04ecb0cef2e3f257534ce0b5bc6cb3242b49d06

--- jenkins-docker/slave-debian/set-java.sh
+++ jenkins-docker/slave-debian/set-java.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
-if [ "$1" == "" ]
-then
-  echo "Set current Java version level"
-  echo ""
-  echo "Use: $0 <7|8>"
-  exit 1
-fi
+usage() {
+    me=`set-java "$0"`
+    echo >&2 "Usage: $me [--toolchain TOOLCHAIN] [JAVA_VER]"
+    exit 1
+}
+
+while test $# -gt 0 ; do
+  case "$1" in
+  --toolchain)
+    shift
+    TOOLCHAIN=$1
+    shift
+    ;;
+
+  *)
+    break
+  esac
+done
+
+test -z "$TOOLCHAIN" && TOOLCHAIN=8
 
 export JAVA_HOME=/usr/lib/jvm/java-$1-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
 
 echo "Java set to: $(which java)"
 
+if test $TOOLCHAIN -ne 8; then
+  export BAZEL_OPTS="--host_javabase=@bazel_tools//tools/jdk:remote_jdk$TOOLCHAIN \
+    --javabase=@bazel_tools//tools/jdk:remote_jdk$TOOLCHAIN \
+    --host_java_toolchain=@bazel_tools//tools/jdk:toolchain_java$TOOLCHAIN \
+    --java_toolchain=@bazel_tools//tools/jdk:toolchain_java$TOOLCHAIN"
+
+  echo "Set Java toolchain $TOOLCHAIN"
+fi
--- jenkins/gerrit-bazel-verifier-test.sh
+++ jenkins/gerrit-bazel-verifier-test.sh
@@ -2,12 +2,12 @@
 
 cd gerrit
 
-. set-java.sh 8
+. set-java.sh --toolchain {java} 8
 
 echo "Test with mode=$MODE"
 echo '----------------------------------------------'
 
-export BAZEL_OPTS="--spawn_strategy=standalone --genrule_strategy=standalone \
+export BAZEL_OPTS="$BAZEL_OPTS --spawn_strategy=standalone --genrule_strategy=standalone \
                  --test_output errors \
                  --test_summary detailed --flaky_test_attempts 3 \
                  --test_verbose_timeout_warnings --build_tests_only \
