225cf8e8ff617340b9202414d5ea0f8cab34d188
Nasser Grainawi <nasser@grainawi.org>
2019-11-16 13:19:29 -0800

    Build the examples plugin
    Build the examples plugin for Gerrit 2.14, 2.15, 2.16, 3.0, 3.1, and
    master.
    
    Use a custom build script since this plugin builds standalone and
    expects to run within the plugin dir.
    
    Change-Id: I92234796eba786ce178ee27ee0ef4bafef94ab2a

---
+++ jenkins/gerrit-bazel-build-examples-plugin.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -e
+
+PLUGIN_PATH=plugins/{name}
+
+git checkout -fb {branch} gerrit/{branch}
+git submodule update --init
+rm -rf "$PLUGIN_PATH"
+git read-tree -u --prefix=$PLUGIN_PATH origin/{branch}
+
+if [ -f "$PLUGIN_PATH"/external_plugin_deps.bzl ]
+then
+  cp -f "$PLUGIN_PATH"/external_plugin_deps.bzl plugins/
+fi
+
+TARGETS=$(echo "{targets}" | sed -e 's/{{name}}/{name}/g')
+
+. set-java.sh 8
+
+java -fullversion
+bazelisk version
+
+pushd "$PLUGIN_PATH"
+bazelisk build --spawn_strategy=standalone --genrule_strategy=standalone all
+
+EXAMPLES=( $(find * -depth 0 -type d -name 'example-*') )
+popd
+
+for EXAMPLE in "${EXAMPLES[@]}"
+do
+    for JAR in $(find "$PLUGIN_PATH"/bazel-bin/"$EXAMPLE" -name "$EXAMPLE".jar)
+    do
+        PLUGIN_VERSION=$(git describe  --always origin/{branch})
+        echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
+        jar ufm $JAR MANIFEST.MF && rm MANIFEST.MF
+        DEST_JAR=bazel-bin/"$PLUGIN_PATH"/$(basename $JAR)
+        [ "$JAR" -ef "$DEST_JAR" ] || mv $JAR $DEST_JAR
+        echo "$PLUGIN_VERSION" > bazel-bin/"$PLUGIN_PATH"/$(basename $JAR-version)
+    done
+done
