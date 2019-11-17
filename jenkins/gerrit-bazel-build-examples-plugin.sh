4a624b319ad7dd85ade4c38d1aabe77057e5a930
Nasser Grainawi <nasser@grainawi.org>
2019-11-16 22:42:29 -0800

    fixup! Build the examples plugin
    Remove unused targets and fix artifacts path.
    
    Change-Id: I457730bf956ab181f08a4e6859215df74d4a6694

--- jenkins/gerrit-bazel-build-examples-plugin.sh
+++ jenkins/gerrit-bazel-build-examples-plugin.sh
@@ -12,28 +12,10 @@ then
   cp -f "$PLUGIN_PATH"/external_plugin_deps.bzl plugins/
 fi
 
-TARGETS=$(echo "{targets}" | sed -e 's/{{name}}/{name}/g')
-
 . set-java.sh 8
 
+pushd "$PLUGIN_PATH"
 java -fullversion
 bazelisk version
-
-pushd "$PLUGIN_PATH"
 bazelisk build --spawn_strategy=standalone --genrule_strategy=standalone all
-
-EXAMPLES=( $(find * -depth 0 -type d -name 'example-*') )
 popd
-
-for EXAMPLE in "${EXAMPLES[@]}"
-do
-    for JAR in $(find "$PLUGIN_PATH"/bazel-bin/"$EXAMPLE" -name "$EXAMPLE".jar)
-    do
-        PLUGIN_VERSION=$(git describe  --always origin/{branch})
-        echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
-        jar ufm $JAR MANIFEST.MF && rm MANIFEST.MF
-        DEST_JAR=bazel-bin/"$PLUGIN_PATH"/$(basename $JAR)
-        [ "$JAR" -ef "$DEST_JAR" ] || mv $JAR $DEST_JAR
-        echo "$PLUGIN_VERSION" > bazel-bin/"$PLUGIN_PATH"/$(basename $JAR-version)
-    done
-done
