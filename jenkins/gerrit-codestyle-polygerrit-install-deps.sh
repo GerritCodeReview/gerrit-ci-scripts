42c3f96485f48d98509b9d056bc4aa3674fb0502
Thomas Draebing <thomas.draebing@sap.com>
2019-12-17 15:53:06 +0100

    Use Gerrit's package.json to create a npm package cache
    The CI installed fixed versions of npm dependencies into the slave
    containers. The versions had to be updated manually, if an update
    was done in the Gerrit project. Thus updating the dependencies
    was quite a slow process.
    
    Now, the npm-packages are installed with yarn from the package.json
    file of the Gerrit project during the build of the slave containers.
    This fills yarn's cache, which will allow to install them during a
    build without having to download them.
    
    The Gerrit-codestyle job was adapted accordingly to use yarn to install
    the required packages. This is tried in offline mode from the cache and
    if that fails the installation is repeated with access to the package
    servers.
    
    The eslint and polylint steps are now run by yarn directly, since the
    scripts run through bazel call npm, which fails, since it does not see
    the dependencies.
    
    Change-Id: I89aa4761f8d449d4caab193361c737b6b71d6d98

--- jenkins/gerrit-codestyle-polygerrit-install-deps.sh
+++ jenkins/gerrit-codestyle-polygerrit-install-deps.sh
@@ -1,15 +1,11 @@
 #!/bin/bash -xe
 cd gerrit
-bazelisk version
 if ([ "$TARGET_BRANCH" == "master" ] || \
     [ "$TARGET_BRANCH" == "stable-3.1" ] || \
     [ "$TARGET_BRANCH" == "stable-3.0" ] || \
     [ "$TARGET_BRANCH" == "stable-2.16" ]) \
     && git show --diff-filter=AM --name-only --pretty="" HEAD | grep polygerrit-ui
 then
-  echo 'Running PolyGerrit lint check...'
-  . set-java.sh 8
-  java -fullversion
-  yarn eslint
-  yarn polylint
+  echo 'Installing required npm packages...'
+  yarn install --offline || yarn install
 fi
