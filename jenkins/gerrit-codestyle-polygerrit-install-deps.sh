15dd4c566cfa8b498c33269cdcf177bba7193400
Luca Milanesio <luca.milanesio@gmail.com>
2020-02-09 21:45:40 +0100

    Revert "Use Gerrit's package.json to create a npm package cache"
    This reverts commit 7e909e85303772f8888239a4c03b3940903f0d16.
    
    Change-Id: I27de4a364dde0cff06c206211760b3fa6fb8664d

--- jenkins/gerrit-codestyle-polygerrit-install-deps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xe
-cd gerrit
-if git show --diff-filter=AM --name-only --pretty="" HEAD | grep polygerrit-ui
-then
-  echo 'Installing required npm packages...'
-  yarn install --offline || yarn install
-fi
