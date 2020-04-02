57f66474467554dd9d02f8daa00017b5d23cc025
David Pursehouse <dpursehouse@collab.net>
2020-04-02 07:13:24 +0000

    Revert "Enable docker-based tests in Gerrit verification build"
    This reverts commit ea284be0932b318dc59d6b6b3522d935731be89d.
    
    Reason for revert: Elasticsearch tests are unstable and cause CI failures.
    
    Change-Id: I409eaaeb7c3c67367da218d485ba58007a0fd86b

--- jenkins/gerrit-bazel-test.sh
+++ jenkins/gerrit-bazel-test.sh
@@ -8,7 +8,7 @@ export BAZEL_OPTS="$BAZEL_OPTS --spawn_strategy=standalone --genrule_strategy=st
                    --test_summary detailed --flaky_test_attempts 3 \
                    --test_verbose_timeout_warnings --build_tests_only \
                    --test_timeout 3600 \
-                   --test_tag_filters=-flaky \
+                   --test_tag_filters=-flaky,-docker \
                    --test_env DOCKER_HOST=$DOCKER_HOST"
 
 java -fullversion
--- jenkins/set-docker-host.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -e
-export DOCKER_HOST=`/sbin/ip route|awk '/default/ {print "tcp://"$3":2375"}'`
