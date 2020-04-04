c187499ec195c006411ed7adcbdba952ef5b4127
David Ostrovsky <david@ostrovsky.org>
2020-04-04 08:33:26 +0200

    Bazel: Activate RBE configuration on GCP
    Change-Id: Ie14a093c38f48e97d386749e9f5801bd516d104e

--- jenkins/gerrit-bazel-build.sh
+++ jenkins/gerrit-bazel-build.sh
@@ -3,7 +3,7 @@
 cd gerrit
 . set-java.sh 8
 
-export BAZEL_OPTS="$BAZEL_OPTS --spawn_strategy=standalone --genrule_strategy=standalone"
+export BAZEL_OPTS="$BAZEL_OPTS"
 
 java -fullversion
 bazelisk version
