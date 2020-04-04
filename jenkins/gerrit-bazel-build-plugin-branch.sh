4e40355d926118aaa8cfe5dcee961fc5271fb22b
David Ostrovsky <david@ostrovsky.org>
2020-04-04 08:33:26 +0200

    Bazel: Activate RBE configuration on GCP
    Change-Id: Ie14a093c38f48e97d386749e9f5801bd516d104e

--- jenkins/gerrit-bazel-build-plugin-branch.sh
+++ jenkins/gerrit-bazel-build-plugin-branch.sh
@@ -21,11 +21,11 @@ BUILD_TARGETS=$(echo "$TARGETS" | tr ' ' '\n' | grep -v test)
 
 java -fullversion
 bazelisk version
-bazelisk build $BAZEL_OPTS --spawn_strategy=standalone --genrule_strategy=standalone $BUILD_TARGETS
+bazelisk build $BAZEL_OPTS $BUILD_TARGETS
 
 if TEST_TARGETS=$(echo "$TARGETS" | tr ' ' '\n' | grep test)
 then
-    BAZEL_OPTS="$BAZEL_OPTS --spawn_strategy=standalone --genrule_strategy=standalone \
+    BAZEL_OPTS="$BAZEL_OPTS \
                    --test_output errors \
                    --test_summary detailed --flaky_test_attempts 3 \
                    --test_verbose_timeout_warnings --build_tests_only \
@@ -37,7 +37,7 @@ fi
 
 for JAR in $(find bazel-bin/plugins/{name} -name {name}*.jar)
 do
-    PLUGIN_VERSION=$(git describe  --always origin/{branch})
+    PLUGIN_VERSION=$(git describe --always origin/{branch})
     echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
     jar ufm $JAR MANIFEST.MF && rm MANIFEST.MF
     DEST_JAR=bazel-bin/plugins/{name}/$(basename $JAR)
