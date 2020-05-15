e7689593cdb78c65fb57bc6063306d9ca7483a80
Luca Milanesio <luca.milanesio@gmail.com>
2020-05-16 00:13:09 +0100

    Remove all jobs but Gerrit verifier for private changes
    The private pipeline is intended to be used for verifying private
    changes only for Gerrit and its plugins.
    
    All other jobs are not useful and would only risk to pollute the
    CI instance with unneeded builds and artifacts.
    
    The gerrit-ci-scripts branch is set to private, so that the pipeline
    is influenced ONLY by changes on its private branch.
    
    Change-Id: Ie498e6ee795547b9f3be8b4f77c164917499928e

--- jenkins/gerrit-bazel-get-scripting-plugin-info.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-curl -L https://gerrit-review.googlesource.com/projects/plugins%2Fscripting%2F{name}/config | \
-     tail -n +2 > bazel-bin/plugins/{name}/{name}.json
-
--- jenkins/gerrit-bazel-gh-get-plugin-info.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-curl -L https://api.github.com/repos/{organization}/{name} \
-     > bazel-bin/plugins/{name}/{name}.json
-
