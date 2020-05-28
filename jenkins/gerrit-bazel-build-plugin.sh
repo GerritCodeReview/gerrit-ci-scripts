a5bb7a83625b81549577314dbad03ff1ac310ea1
Luca Milanesio <luca.milanesio@gmail.com>
2020-05-28 13:04:12 +0100

    Do not find plugins JARS in bazel-bin subdirectories
    Fix the detection of the plugin's target jar by explicitly
    looking in bazel-bin/plugins/{name}/{name}.jar.
    
    If the plugin had somewhere in its nested structure another {name}.jar
    file generated (e.g. checks tests) it would have confused the collection
    of the resulting artifact and archived the tests instead of the plugin
    jar file.
    
    Bug: Issue 12817
    Change-Id: I5afc801aa630d3040371f104ca92cd2b415e6913

--- jenkins/gerrit-bazel-build-plugin.sh
+++ jenkins/gerrit-bazel-build-plugin.sh
@@ -19,12 +19,8 @@ bazelisk version
 bazelisk build --spawn_strategy=standalone --genrule_strategy=standalone $TARGETS
 bazelisk test --test_env DOCKER_HOST=$DOCKER_HOST //tools/bzl:always_pass_test plugins/{name}/...
 
-for JAR in $(find bazel-bin/plugins/{name} -name {name}*.jar)
-do
-    PLUGIN_VERSION=$(git describe  --always origin/{branch})
-    echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
-    jar ufm $JAR MANIFEST.MF && rm MANIFEST.MF
-    DEST_JAR=bazel-bin/plugins/{name}/$(basename $JAR)
-    [ "$JAR" -ef "$DEST_JAR" ] || mv $JAR $DEST_JAR
-    echo "$PLUGIN_VERSION" > bazel-bin/plugins/{name}/$(basename $JAR-version)
-done
+JAR="bazel-bin/plugins/{name}/{name}.jar"
+PLUGIN_VERSION=$(git describe  --always origin/{branch})
+echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
+jar ufm $JAR MANIFEST.MF && rm MANIFEST.MF
+echo "$PLUGIN_VERSION" > bazel-bin/plugins/{name}/$(basename $JAR-version)
