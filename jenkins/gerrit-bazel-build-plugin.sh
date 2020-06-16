6a2ec1ab541ba5a1634c0f5399b8c1ff3934f251
Thomas Draebing <thomas.draebing@sap.com>
2020-06-16 17:32:34 +0200

    Add job for image-diff plugin
    This also adds a job template to build UI-only plugins. These are
    plugins only providing html and js files, but no jar file. The
    default plugin build job was not usable for these artifacts, since
    it artifact archiver was expecting a jar-file and would fail, if
    it was missing.
    
    Change-Id: I4e80f1cb4604c3d0c842c8cacdbcc71906d31300

--- jenkins/gerrit-bazel-build-plugin.sh
+++ jenkins/gerrit-bazel-build-plugin.sh
@@ -23,7 +23,10 @@ bazelisk build --spawn_strategy=standalone --genrule_strategy=standalone $TARGET
 bazelisk test --test_env DOCKER_HOST=$DOCKER_HOST //tools/bzl:always_pass_test plugins/{name}/...
 
 JAR="bazel-bin/plugins/{name}/{name}.jar"
-PLUGIN_VERSION=$(git describe  --always origin/{branch})
-echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
-jar ufm $JAR MANIFEST.MF && rm MANIFEST.MF
-echo "$PLUGIN_VERSION" > bazel-bin/plugins/{name}/$(basename $JAR-version)
+if test -f $JAR
+then
+  PLUGIN_VERSION=$(git describe  --always origin/{branch})
+  echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
+  jar ufm $JAR MANIFEST.MF && rm MANIFEST.MF
+  echo "$PLUGIN_VERSION" > bazel-bin/plugins/{name}/$(basename $JAR-version)
+fi
