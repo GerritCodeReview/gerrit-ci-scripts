5ddb14260322d620ca37bca4f9dfe29d6afa2ff7
David Ostrovsky <david@ostrovsky.org>
2020-07-07 22:16:19 +0200

    Remove Polymer lint tests on some branches
    Alternative approach would be to remove the tests entirely.
    
    Change-Id: I0ac81a385c9bb548035debc7d176a1673e9b5c10

--- jenkins/gerrit-codestyle-polygerrit-lint.sh
+++ jenkins/gerrit-codestyle-polygerrit-lint.sh
@@ -1,11 +1,8 @@
 #!/bin/bash -xe
 cd gerrit
 bazelisk version
-if ([ "$TARGET_BRANCH" == "master" ] || \
-    [ "$TARGET_BRANCH" == "stable-3.2" ] || \
-    [ "$TARGET_BRANCH" == "stable-3.1" ] || \
-    [ "$TARGET_BRANCH" == "stable-3.0" ] || \
-    [ "$TARGET_BRANCH" == "stable-2.16" ]) \
+if ([ "$TARGET_BRANCH" == "stable-3.1" ] || \
+    [ "$TARGET_BRANCH" == "stable-3.0" ]) \
     && git show --diff-filter=AM --name-only --pretty="" HEAD | grep polygerrit-ui
 then
   echo 'Running PolyGerrit lint check...'
