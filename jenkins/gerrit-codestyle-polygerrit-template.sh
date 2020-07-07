0dead1760538b1583bfdab65409e46b30a16be70
David Ostrovsky <david@ostrovsky.org>
2020-07-07 22:14:56 +0200

    Remove Polymer template tests in stable-3.2 and later
    Change-Id: Iee528470ace8583bdb80ca9fd327e0aa659457f3

--- jenkins/gerrit-codestyle-polygerrit-template.sh
+++ jenkins/gerrit-codestyle-polygerrit-template.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -xe
 cd gerrit
-if ([ "$TARGET_BRANCH" == "master" ] || \
-    [ "$TARGET_BRANCH" == "stable-3.2" ] || \
-    [ "$TARGET_BRANCH" == "stable-3.1" ] || \
+if ([ "$TARGET_BRANCH" == "stable-3.1" ] || \
     [ "$TARGET_BRANCH" == "stable-3.0" ] || \
     [ "$TARGET_BRANCH" == "stable-2.16" ]) \
     && git show --diff-filter=AM --name-only --pretty="" HEAD | grep polygerrit-ui
