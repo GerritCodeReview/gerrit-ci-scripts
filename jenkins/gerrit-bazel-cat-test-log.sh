096e0cdb57bd0e0b9e87375f2c00a818e61922da
Luca Milanesio <luca.milanesio@gmail.com>
2020-07-08 08:41:42 +0100

    Find and display all test.log upon PolyGerrit failures
    When a PolyGerrit test fails, find and display all the relevant
    test.log, so that it can be accessed later on.
    
    Different testing frameworks (e.g. WCT vs. Karma) may generate
    the test.log in different subdirectories and therefore it is not
    possible anymore to define a specific pattern for the log file.
    
    Using a generic 'find' would assure that whatever is the testing
    framework used, the test.log would always be found.
    
    Bug: Issue 13111
    Change-Id: Iddbda098610ba31202b49011358bf4d93c290305

--- jenkins/gerrit-bazel-cat-test-log.sh
+++ jenkins/gerrit-bazel-cat-test-log.sh
@@ -7,7 +7,7 @@ then
    echo "See test log below"
    echo "=============================="
    echo ""
-   cat $(ls ~/.cache/bazel/_bazel_jenkins/*/execroot/gerrit/bazel-out/*/testlogs/polygerrit-ui/app/wct_test/test.log)
+   find ~/.cache/bazel/_bazel_jenkins -name test.log -exec cat {{}} \;
    exit -1
 fi
 
