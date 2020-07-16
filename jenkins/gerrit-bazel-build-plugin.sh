8ff9622b0aef4c0fd79c4f114b9bfcd402458ae3
Marcin Czech <maczech@gmail.com>
2020-07-16 18:34:48 +0200

    Add multiple targets build for zookeeper-refdb plugin
    Zookeeper-refdb plugin builds with two zookeeper
    versions(3.4.x and 3.5.x)
    
    Feature: Issue 12583
    Change-Id: Ia62c8bbfe1c31bce197aae52bc0070881cef09aa

--- jenkins/gerrit-bazel-build-plugin.sh
+++ jenkins/gerrit-bazel-build-plugin.sh
@@ -22,11 +22,12 @@ bazelisk version
 bazelisk build --spawn_strategy=standalone --genrule_strategy=standalone $TARGETS
 bazelisk test --test_env DOCKER_HOST=$DOCKER_HOST //tools/bzl:always_pass_test plugins/{name}/...
 
-JAR="bazel-bin/plugins/{name}/{name}.jar"
-if test -f $JAR
-then
-  PLUGIN_VERSION=$(git describe  --always origin/{branch})
-  echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
-  jar ufm $JAR MANIFEST.MF && rm MANIFEST.MF
-  echo "$PLUGIN_VERSION" > bazel-bin/plugins/{name}/$(basename $JAR-version)
-fi
+for JAR in $(find bazel-bin/plugins/{name} -name {name}*.jar)
+do
+    PLUGIN_VERSION=$(git describe  --always origin/{branch})
+    echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
+    jar ufm $JAR MANIFEST.MF && rm MANIFEST.MF
+    DEST_JAR=bazel-bin/plugins/{name}/$(basename $JAR)
+    [ "$JAR" -ef "$DEST_JAR" ] || mv $JAR $DEST_JAR
+    echo "$PLUGIN_VERSION" > bazel-bin/plugins/{name}/$(basename $JAR-version)
+done
