93105d84406f81c0a5b0abaace0d8faab6afe8b2
Luca Milanesio <luca.milanesio@gmail.com>
2020-10-06 08:48:07 +0100

    Disable elasticsearch tests in Gerrit-CI builds
    Because of recent instability, the elasticsearch tests are
    blocking the CI pipeline and making the build fail for timeout.
    
    Disable all the tests flagged with 'elastic' until a solution
    is found.
    
    Bug: Issue 13474
    Change-Id: I888d9ffe544b3c3725df6701a98356d608fa4793

--- jenkins/gerrit-bazel-test.sh
+++ jenkins/gerrit-bazel-test.sh
@@ -8,7 +8,7 @@ export BAZEL_OPTS="$BAZEL_OPTS --spawn_strategy=standalone --genrule_strategy=st
                    --test_summary detailed --flaky_test_attempts 3 \
                    --test_verbose_timeout_warnings --build_tests_only \
                    --test_timeout 3600 \
-                   --test_tag_filters=-flaky \
+                   --test_tag_filters=-flaky,-elastic \
                    --test_env DOCKER_HOST=$DOCKER_HOST"
 export WCT_HEADLESS_MODE=1
 
