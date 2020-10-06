ef00f1f9d72f5556b8c04eae032d74b63547dc76
Luca Milanesio <luca.milanesio@gmail.com>
2020-10-06 08:48:07 +0100

    Disable elasticsearch tests in Gerrit-CI builds
    Because of recent instability, the elasticsearch tests are
    blocking the CI pipeline and making the build fail for timeout.
    
    Disable all the tests flagged with 'elastic' on the stable-2.16
    branch, until a solution is found.
    
    Bug: Issue 13474
    Change-Id: I888d9ffe544b3c3725df6701a98356d608fa4793

--- jenkins/gerrit-bazel-build-plugin-branch.sh
+++ jenkins/gerrit-bazel-build-plugin-branch.sh
@@ -24,13 +24,20 @@ java -fullversion
 bazelisk version
 bazelisk build $BAZEL_OPTS --spawn_strategy=standalone --genrule_strategy=standalone $TARGETS
 
+TEST_TAG_FILTERS="-flaky"
+if [ "{branch}" == "stable-2.16" ]
+then
+  TEST_TAG_FILTERS=$TEST_TAG_FILTERS",-elastic"
+fi
+
 BAZEL_OPTS="$BAZEL_OPTS --spawn_strategy=standalone --genrule_strategy=standalone \
                    --test_output errors \
                    --test_summary detailed --flaky_test_attempts 3 \
                    --test_verbose_timeout_warnings --build_tests_only \
                    --test_timeout 3600 \
-                   --test_tag_filters=-flaky \
+                   --test_tag_filters=$TEST_TAG_FILTERS \
                    --test_env DOCKER_HOST=$DOCKER_HOST"
+
 bazelisk test $BAZEL_OPTS //tools/bzl:always_pass_test plugins/{name}/...
 
 for JAR in $(find bazel-bin/plugins/{name} -name {name}*.jar)
