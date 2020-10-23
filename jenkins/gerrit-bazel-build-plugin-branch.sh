85a334735745e1e60544342aba6231bbcd732dd6
Luca Milanesio <luca.milanesio@gmail.com>
2020-10-24 00:07:48 +0100

    Build plugins on Java 11 for stable-3.3/master
    Gerrit stable-3.3 and master is distributed on Java 11
    and the plugin API won't be usable from Java 8 as they are
    using the Major version 55.
    
    Do NOT enforce anymore a specific toolchain but let
    Bazel use the local JVM.
    
    Change-Id: Ic949afaf0f479d6486e7de712599ff2cbbb05b9f

--- jenkins/gerrit-bazel-build-plugin-branch.sh
+++ jenkins/gerrit-bazel-build-plugin-branch.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -e
 
-. set-java.sh 8
+case "{branch}" in
+  stable-2.16|stable-3.0|stable-3.1|stable-3.2)
+    . set-java.sh 8
+    ;;
+  *)
+    . set-java.sh 11
+    ;;
+esac
 
 echo "Building plugin {name}/{branch} with Gerrit/{gerrit-branch}"
 
