f9cdda552ae72c5a31311a76c8f9bca94257f020
Luca Milanesio <luca.milanesio@gmail.com>
2020-10-27 22:54:05 +0000

    Rebuild and re-test everything uncached when upgrading Bazel
    Bazel version bump should trigger a complete revalidation of
    everything without having to rely on local or remote caches.
    
    Also resete BAZEL_OPTS to prevent the use of remote cache
    enabled at project-level definition.
    
    Change-Id: If3605b5bd6b3cc0b4d82b0ba55c46eb743d4c064

--- jenkins/gerrit-codestyle-polygerrit-lint.sh
+++ jenkins/gerrit-codestyle-polygerrit-lint.sh
@@ -2,13 +2,19 @@
 
 . set-java.sh 8
 
+if [git show --diff-filter=AM --name-only --pretty="" HEAD | grep -q .bazelversion]
+then
+  export BAZEL_OPTS=""
+fi
+
 cd gerrit
 bazelisk version
 if ([ "$TARGET_BRANCH" == "master" ] || \
     [ "$TARGET_BRANCH" == "stable-3.3" ] || \
     [ "$TARGET_BRANCH" == "stable-3.2" ]) && \
-   ((git show --diff-filter=AM --name-only --pretty="" HEAD | grep polygerrit-ui) || \
-    (git show --summary HEAD | grep -q ^Merge:))
+   ((git show --diff-filter=AM --name-only --pretty="" HEAD | grep -q polygerrit-ui) || \
+    (git show --summary HEAD | grep -q ^Merge:) || \
+    (git show --diff-filter=AM --name-only --pretty="" HEAD | grep -q .bazelversion))
 then
   echo 'Running PolyGerrit lint check...'
   java -fullversion
