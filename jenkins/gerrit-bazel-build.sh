1420a45c40417425b164cfb82ddc8a0e6cb8d24a
Luca Milanesio <luca.milanesio@gmail.com>
2020-10-27 22:54:05 +0000

    Rebuild and re-test everything uncached when upgrading Bazel
    Bazel version bump should trigger a complete revalidation of
    everything without having to rely on local or remote caches.
    
    Also reset BAZEL_OPTS to prevent the use of remote cache
    enabled at project-level definition.
    
    Change-Id: If3605b5bd6b3cc0b4d82b0ba55c46eb743d4c064

--- jenkins/gerrit-bazel-build.sh
+++ jenkins/gerrit-bazel-build.sh
@@ -4,6 +4,11 @@
 
 cd gerrit
 
+if [git show --diff-filter=AM --name-only --pretty="" HEAD | grep -q .bazelversion]
+then
+  export BAZEL_OPTS=""
+fi
+
 export BAZEL_OPTS="$BAZEL_OPTS --spawn_strategy=standalone --genrule_strategy=standalone"
 
 java -fullversion
