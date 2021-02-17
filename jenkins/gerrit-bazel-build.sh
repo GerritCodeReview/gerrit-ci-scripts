ce80619eea24cced8ac584e42ae7caa3bc8a14c6
Luca Milanesio <luca.milanesio@gmail.com>
2021-02-17 00:39:18 +0000

    Revert "Bazel: Activate RBE configuration on GCP"
    This reverts commit d9216e8bebc8968330f7767e73b45e281c005249.
    
    Reason for revert: RBE build fails with the following error.
    Error in fail: Target 'rbe_jdk11' failed: use_checked_in_confs was set to 'Force' but no checked-in configs were found. Please check your pin to '@bazel_toolchains' is up to date, and that you are using a release version of Bazel. You can also explicitly set the version of Bazel to an older version in the 'rbe_jdk11' rbe_autoconfig target which may or may not work with the version you are currently running with.
    
    Change-Id: I4272784ad5fdf6b910cd69d8ee647e5844bc167c

--- jenkins/gerrit-bazel-build.sh
+++ jenkins/gerrit-bazel-build.sh
@@ -4,9 +4,6 @@
 
 cd gerrit
 
-echo "Build with mode=$MODE"
-echo '----------------------------------------------'
-
 if git show --diff-filter=AM --name-only --pretty="" HEAD | grep -q .bazelversion
 then
   export BAZEL_OPTS=""
@@ -14,14 +11,6 @@ fi
 
 java -fullversion
 bazelisk version
-
-if [[ "$MODE" == *"rbe"* ]]
-then
-    # TODO(davido): Figure out why javadoc part of api-rule doesn't work on RBE.
-    # See: https://github.com/bazelbuild/bazel/issues/12765 for more background.
-  bazelisk build --config=remote --remote_instance_name=projects/api-project-164060093628/instances/default_instance plugins:core release api-skip-javadoc
-else
-  bazelisk build $BAZEL_OPTS plugins:core release api
-  tools/maven/api.sh install
-  tools/eclipse/project.py --bazel bazelisk
-fi
+bazelisk build $BAZEL_OPTS plugins:core release api
+tools/maven/api.sh install
+tools/eclipse/project.py --bazel bazelisk
