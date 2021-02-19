e931c9834ab8f25404c612f5075add02bbb0627f
David Ostrovsky <david@ostrovsky.org>
2021-02-19 19:40:33 +0100

    Remove --test_summary=detailed option
    It seems that bazel doesn't report whether or not test result was
    cached execution was skipped, when this option is passed so switch
    to the default (short), e.g.:
    
      //javatests/com/google/gerrit/acceptance/server/mail:MailIT \
      (cached) PASSED in 19.6s
    
    Change-Id: Ib8c6580058ab458ea8f127821e623f7882858fd7

--- jenkins/gerrit-bazel-verifier-test.sh
+++ jenkins/gerrit-bazel-verifier-test.sh
@@ -24,7 +24,7 @@ esac
 
 export BAZEL_OPTS="$BAZEL_OPTS \
                  --test_output errors \
-                 --test_summary detailed --flaky_test_attempts 3 \
+                 --flaky_test_attempts 3 \
                  --test_verbose_timeout_warnings --build_tests_only \
                  --test_timeout 3600 \
                  --test_tag_filters=$TEST_TAG_FILTER \
