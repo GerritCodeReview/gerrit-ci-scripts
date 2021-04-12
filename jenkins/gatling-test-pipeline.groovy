839711b95a1ff8ede8facffdc6d3df84af73ebcd
Luca Milanesio <luca.milanesio@gmail.com>
2021-04-12 19:15:15 +0100

    Gatling tests: bump Gerrit to v3.4.0-rc1
    Change-Id: I5680a1f20f691abe9b844ad1c869ee7bac023bd1

--- jenkins/gatling-test-pipeline.groovy
+++ jenkins/gatling-test-pipeline.groovy
@@ -10,7 +10,7 @@ pipeline {
             string(name: 'AWS_REGION', defaultValue:"us-east-1", description: 'Which region to deploy to')
 
             string(name: 'GERRIT_VERSION', defaultValue:"3.4", description: 'The gerrit version under test')
-            string(name: 'GERRIT_PATCH', defaultValue:"0-rc0", description: 'The gerrit version patch under test')
+            string(name: 'GERRIT_PATCH', defaultValue:"0-rc1", description: 'The gerrit version patch under test')
 
             string(name: 'HOSTED_ZONE_NAME', defaultValue: "gerritforgeaws.com", description: 'Name of the hosted zone')
             string(name: 'CLUSTER_INSTANCE_TYPE', defaultValue: 'm4.xlarge', description:'The EC2 instance Type used to run the cluster')
