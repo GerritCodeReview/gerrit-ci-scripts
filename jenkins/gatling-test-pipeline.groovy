9fe921f3682fd75d9d39fe218e15dbab832cd2a3
Luca Milanesio <luca.milanesio@gmail.com>
2021-04-27 01:33:50 +0100

    Test Gerrit v3.4.0-rc3 with Gatling
    Set Gerrit veresion to test to the latest v3.4.0-rc3
    
    Change-Id: Ifcfe07981601af2876ec6ba4c5b0aca8ae6dea45

--- jenkins/gatling-test-pipeline.groovy
+++ jenkins/gatling-test-pipeline.groovy
@@ -10,7 +10,7 @@ pipeline {
             string(name: 'AWS_REGION', defaultValue:"us-east-1", description: 'Which region to deploy to')
 
             string(name: 'GERRIT_VERSION', defaultValue:"3.4", description: 'The gerrit version under test')
-            string(name: 'GERRIT_PATCH', defaultValue:"0-rc1", description: 'The gerrit version patch under test')
+            string(name: 'GERRIT_PATCH', defaultValue:"0-rc3", description: 'The gerrit version patch under test')
             string(name: 'GERRIT_WAR_URL', defaultValue:"https://gerrit-ci.gerritforge.com/job/Gerrit-bazel-java11-stable-3.4/lastSuccessfulBuild/artifact/gerrit/bazel-bin/release.war", description: 'The gerrit.war URL to use as override of the gerrit version under test')
 
             string(name: 'HOSTED_ZONE_NAME', defaultValue: "gerritforgeaws.com", description: 'Name of the hosted zone')
