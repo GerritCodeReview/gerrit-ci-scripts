a9619b84c395711eeeae4581990e1475a04857a3
Luca Milanesio <luca.milanesio@gmail.com>
2021-09-07 09:48:44 +0100

    Add TrackingIdInfo to the classes allow-list
    Jenkins rejected the Gerrit SCM serialisation at [1]
    and needs an explicit allow-list as documented at [2].
    
    [1] https://gerrit-ci.gerritforge.com/view/Gerrit/job/Gerrit-verifier-pipeline/view/change-requests/job/57%252F317257%252F2/1/console
    [2] https://groups.google.com/g/jenkinsci-dev/c/1-46eS41y6I
    
    Change-Id: I65f49872555cbbbce154f1d993cc8fefcab9a2fb

--- jenkins-docker/master/jenkins.sh
+++ jenkins-docker/master/jenkins.sh
@@ -6,7 +6,11 @@ rm -Rf /var/jenkins_home/nodes
 chown -R jenkins:dockergroup /var/jenkins_home
 usermod -G docker jenkins
 
-ALLOW_REMOTING_CLASSES=com.google.gerrit.extensions.common.AvatarInfo,com.google.gerrit.extensions.common.ReviewerUpdateInfo
+ALLOW_REMOTING_CLASSES=\
+com.google.gerrit.extensions.common.AvatarInfo,\
+com.google.gerrit.extensions.common.ReviewerUpdateInfo,\
+com.google.gerrit.extensions.common.TrackingIdInfo
+
 JAVA_OPTS="-Dfile.encoding=UTF-8 -Dhudson.remoting.ClassFilter=$ALLOW_REMOTING_CLASSES"
 
 gosu jenkins bash -c "JAVA_OPTS='$JAVA_OPTS' /usr/local/bin/run-jenkins.sh $*"
