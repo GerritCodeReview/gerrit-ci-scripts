05e838f2cd7c5fb1e3a2fa0cdd475e65bb3ee3c3
Luca Milanesio <luca.milanesio@gmail.com>
2021-12-21 20:52:38 +0000

    Disable change validation on RBE
    Change validations on RBE started to become very slow and flaky
    since the 15th of December. See two example of the problems on
    [1] and [2].
    
    Waiting to dig deeper into the RBE issues, disable the RBE
    execution and rely on local bazel builds.
    
    [1] https://gerrit-review.googlesource.com/c/gerrit/+/326417
    [2] https://gerrit-review.googlesource.com/c/gerrit/+/324075
    
    Change-Id: I8b7a724f44d62c6a26673c4a2ec47be3bf7a8d4d

--- jenkins/gerrit-bazel-build.sh
+++ jenkins/gerrit-bazel-build.sh
@@ -26,7 +26,13 @@ if [[ "$MODE" == *"rbe"* ]]
 then
     # TODO(davido): Figure out why javadoc part of api-rule doesn't work on RBE.
     # See: https://github.com/bazelbuild/bazel/issues/12765 for more background.
-  bazelisk build --config=remote --remote_instance_name=projects/api-project-164060093628/instances/default_instance plugins:core release api-skip-javadoc
+    # TODO(luca): RBE should be disabled because of instability with the following suites:
+    # //javatests/com/google/gerrit/server:server_tests
+    # //javatests/com/google/gerrit/pgm:pgm_tests
+    # The above two tests take hours on RBE, which is inexplicable as they are supposed
+    # to take just a few minutes
+    # bazelisk build --config=remote --remote_instance_name=projects/api-project-164060093628/instances/default_instance plugins:core release api-skip-javadoc
+  bazelisk build $BAZEL_OPTS plugins:core release api-skip-javadoc
 else
   bazelisk build $BAZEL_OPTS plugins:core release api
   tools/maven/api.sh install
