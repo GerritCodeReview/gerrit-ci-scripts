b37a0a7fb3e87b3ce8fccf91e40084bbb7cc96e5
Fabio Ponciroli <ponch78@gmail.com>
2022-09-21 17:26:16 -0500

    Basic polumi project structure
    Created the skeleton of a Pulumi project.
    Defined convention for the component structure, stacks
    and bucket stack state.
    
    Change-Id: I1e8a488d6e559e2c2b173db193f79433dd48cf81

---
+++ jenkins-docker/server-pulumi/__main__.py
@@ -0,0 +1,35 @@
+"""Docker image for Jenkins server"""
+
+import fileinput
+import pulumi
+import pulumi_docker as docker
+from pulumi_docker import DockerBuild
+
+stack = pulumi.get_stack()
+
+JENKINS_SERVER_IMAGE_NAME = "jenkins-server"
+JENKINS_VERSION = "2.222.4"
+JENKINS_WAR_SHA = "924d2c9fabfdcacee1bae757337a07d7599eaa35"
+JENKINS_WAR_VER = "2.204.1"
+GF_JENKINS_SERVER_VERSION = "0.1"
+
+
+def _prepare_dockerfile():
+    with open('Dockerfile.template', 'r') as file:
+        filedata = file.read()
+
+    filedata = filedata.replace('_JENKINS_WAR_VER_', JENKINS_WAR_VER)
+    filedata = filedata.replace('_JENKINS_WAR_SHA_', JENKINS_WAR_SHA)
+
+    with open('Dockerfile', 'w') as file:
+        file.write(filedata)
+
+
+_prepare_dockerfile()
+
+backend = docker.Image(
+    name="jenkins_server",
+    build=DockerBuild(),
+    local_image_name="gerritforge/jenkins-server:" + GF_JENKINS_SERVER_VERSION,
+    image_name="gerritforge/jenkins-server:" + GF_JENKINS_SERVER_VERSION,
+    skip_push=True)
