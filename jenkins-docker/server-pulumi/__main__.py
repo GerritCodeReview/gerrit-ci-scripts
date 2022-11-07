b605df4a5543c0c9464214b451e5e09887e6fdee
Darius Jokilehto <dariusjokilehto+os@gmail.com>
2022-11-07 14:42:23 +0000

    wip
    Change-Id: Idd1370964d5d830607bb403bf2c0f3c1004827c7

--- jenkins-docker/server-pulumi/__main__.py
+++ jenkins-docker/server-pulumi/__main__.py
@@ -1,6 +1,5 @@
 """Docker image for Jenkins server"""
 
-import fileinput
 from pathlib import Path
 
 import pulumi
@@ -11,30 +10,21 @@ stack = pulumi.get_stack()
 
 JENKINS_SERVER_IMAGE_NAME = "jenkins-server"
 JENKINS_VERSION = "2.222.4"
-JENKINS_WAR_SHA = "924d2c9fabfdcacee1bae757337a07d7599eaa35"
-JENKINS_WAR_VER = "2.204.1"
+JENKINS_WAR_VER = "2.222.4"
+# https://get.jenkins.io/war-stable/2.222.4/
+JENKINS_WAR_SHA = "6c95721b90272949ed8802cab8a84d7429306f72b180c5babc33f5b073e1c47c"
 GF_JENKINS_SERVER_VERSION = "0.1"
 
 cwd = Path(__file__).parent
 build_dir = cwd / 'build'
 
-
-def _prepare_dockerfile():
-    with open(cwd / 'Dockerfile.template', 'r') as file:
-        filedata = file.read()
-
-    filedata = filedata.replace('_JENKINS_WAR_VER_', JENKINS_WAR_VER)
-    filedata = filedata.replace('_JENKINS_WAR_SHA_', JENKINS_WAR_SHA)
-
-    with open(build_dir / 'Dockerfile', 'w') as file:
-        file.write(filedata)
-
-
-_prepare_dockerfile()
-
 backend = docker.Image(
     name="jenkins_server",
-    build=DockerBuild(context=str(build_dir)),
+    build=DockerBuild(context=str(build_dir),
+                      args={
+                          "JENKINS_WAR_VER": JENKINS_WAR_VER,
+                          "JENKINS_WAR_SHA": JENKINS_WAR_SHA
+                      }),
     local_image_name="gerritforge/jenkins-server:" + GF_JENKINS_SERVER_VERSION,
     image_name="gerritforge/jenkins-server:" + GF_JENKINS_SERVER_VERSION,
     skip_push=True)
