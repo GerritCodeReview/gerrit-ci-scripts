f4e9d94e097a4e7da8cad39ce9959463e3b0e11e
Fabio Ponciroli <ponch78@gmail.com>
2023-04-25 11:15:42 +0200

    Include pull-replication in multi-site build
    Change-Id: I259dd94719cbc101eaae15d9085901f8668fd171

---
+++ jenkins/gerrit-bazel-build-multi-site.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+
+. set-java.sh 11
+
+git checkout -fb {branch} gerrit/{branch}
+git submodule update --init
+rm -rf plugins/{name}
+rm -rf plugins/pull-replication
+git read-tree -u --prefix=plugins/{name}-plugin origin/{branch}
+git read-tree -u --prefix=plugins/pull-replication-plugin origin/{branch}
+git fetch --tags origin
+
+for file in external_plugin_deps.bzl
+do
+  if [ -f plugins/{name}-plugin/$file ]
+  then
+    cp -f plugins/{name}-plugin/$file plugins/
+  fi
+done
+
+java -fullversion
+bazelisk clean
+bazelisk version
+bazelisk build $BAZEL_OPTS plugins/multi-site:multi-site
+bazelisk test $BAZEL_OPTS --test_env DOCKER_HOST=$DOCKER_HOST plugins/multi-site:multi-site/...
+
+for JAR in $(find bazel-bin/plugins/ -name {name}*.jar | egrep -e '(stamped|tests|header)' -v)
+do
+    PLUGIN_VERSION=$(git describe  --always origin/{branch})
+    echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
+    jar ufm $JAR MANIFEST.MF && rm MANIFEST.MF
+    DEST_JAR=bazel-bin/plugins/{name}/$(basename $JAR)
+    [ "$JAR" -ef "$DEST_JAR" ] || mv $JAR $DEST_JAR
+    echo "$PLUGIN_VERSION" > bazel-bin/plugins/{name}/$(basename $JAR-version)
+done
