4fe35dee8bd9ba639ba6511fe70b9ed6e2bd0c6b
Luca Milanesio <luca.milanesio@gmail.com>
2023-12-29 15:16:37 +0000

    Make the GJF output more prominent in Code-Style
    Add a more preminent title and explanatory message
    to highlight the list of files that have formatting
    issues in the Code-Style job script.
    
    Change-Id: Ida9349b4becfd11124d3a8510cd278fd18213e3a

--- jenkins/gerrit-codestyle-gjf.sh
+++ jenkins/gerrit-codestyle-gjf.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 cd gerrit
 GJF_VERSION=$(grep -o "^VERSION=.*$" tools/setup_gjf.sh | grep -o "[0-9][0-9]*\.[0-9][0-9]*")
 GJF="/home/jenkins/format/google-java-format-$GJF_VERSION"
@@ -6,5 +6,9 @@ if [ ! -f "$GJF" ]; then
   ./tools/setup_gjf.sh
   GJF=$(find 'tools/format' -regex '.*/google-java-format-[0-9][0-9]*\.[0-9][0-9]*')
 fi
-echo 'Running google-java-format check...'
-git show --diff-filter=AM --name-only --pretty="" HEAD | grep java$ | xargs -r $GJF -n --set-exit-if-changed
+
+echo ""
+echo "==============================================================="
+echo "Running google-java-format check ...                           "
+echo "---------------------------------------------------------------"
+O=$(git show --diff-filter=AM --name-only --pretty="" HEAD | grep java$ | xargs -r $GJF -n --set-exit-if-changed) || printf "The following files are not properly formatted:\n$O"
