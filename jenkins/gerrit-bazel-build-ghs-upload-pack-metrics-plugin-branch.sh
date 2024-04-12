825cc757fbb8ea2b93d413122db5444644ebd531
Jacek Centkowski <geminica.programs@gmail.com>
2024-04-12 10:02:46 +0200

    Add ghs-upload-pack-metrics plugin build to Gerrit CI
    Closes #40
    
    Change-Id: Id26bf21cbe2eefe6d6ed02ef4aa2e56b30c724e7

---
+++ jenkins/gerrit-bazel-build-ghs-upload-pack-metrics-plugin-branch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+. set-java.sh 17
+
+git checkout -fb {branch} gerrit/{gerrit-branch}
+git submodule update --init
+git read-tree -u --prefix=plugins/{name} origin/{branch}
+git fetch --tags origin
+pushd plugins && ln -s gerritforge-health-service/components/collectors/gerrit/ghs-upload-pack-metrics . && popd
+
+TARGETS=$(echo "{targets}" | sed -e 's/{{name}}/{name}/g')
+
+java -fullversion
+bazelisk version
+bazelisk build $TARGETS
+
+for target in $TARGETS
+do
+    bazelisk test $target/... //tools/bzl:always_pass_test
+done
+
+for JAR in $(find bazel-bin/plugins/ -name {name}*.jar | egrep -e '(stamped|tests|header)' -v)
+do
+    PLUGIN_VERSION=$(git describe  --always origin/{branch})
+    echo -e "Implementation-Version: $PLUGIN_VERSION" > MANIFEST.MF
+    jar ufm $JAR MANIFEST.MF && rm MANIFEST.MF
+    DEST_JAR=bazel-bin/plugins/{name}/$(basename $JAR)
+    [ "$JAR" -ef "$DEST_JAR" ] || mv $JAR $DEST_JAR
+    echo "$PLUGIN_VERSION" > bazel-bin/plugins/{name}/$(basename $JAR-version)
+done
