4633ab6fdb61651d5399dfc721a3ec92be03b10f
Paladox none <thomasmulhall410@yahoo.com>
2024-10-21 15:49:44 +0000

    Build Gerrit on stable-3.11 branches
    Change-Id: I57f6ac7afbf31ebbee744dd714d1039cc92472a1

--- jenkins/gerrit-bazel-verifier-test.sh
+++ jenkins/gerrit-bazel-verifier-test.sh
@@ -8,11 +8,11 @@ echo "Test with mode=$MODE"
 echo '----------------------------------------------'
 
 case $TARGET_BRANCH$MODE in
-  masterrbe|stable-3.8rbe|stable-3.9rbe|stable-3.10rbe)
+  masterrbe|stable-3.8rbe|stable-3.9rbe|stable-3.10rbe|stable-3.11rbe)
     TEST_TAG_FILTER="-flaky,-elastic,-no_rbe"
     BAZEL_OPTS="$BAZEL_OPTS --config=remote_bb --jobs=50 --remote_header=x-buildbuddy-api-key=$BB_API_KEY"
     ;;
-  masternotedb|stable-3.8notedb|stable-3.9notedb|stable-3.10notedb)
+  masternotedb|stable-3.8notedb|stable-3.9notedb|stable-3.10notedb|stable-3.11notedb)
     TEST_TAG_FILTER="-flaky,elastic,no_rbe"
     ;;
   stable-2.*)
