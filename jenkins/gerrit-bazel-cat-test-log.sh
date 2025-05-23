ffc647b93e34556c926131194f9d7ae1c21f8c49
Paladox none <thomasmulhall410@yahoo.com>
2025-05-23 09:43:13 -0700

    Fix printing output of pg log
    It's now `testlogs/polygerrit-ui/web_test_runner/test.log`.
    Not `testlogs/polygerrit-ui/karma_test/test.log` or
    `testlogs/polygerrit-ui/wct_test/test.log`.
    
    Change-Id: Ibb66cae18e4e123ad94e8fb01c174321fe64f0f3

--- jenkins/gerrit-bazel-cat-test-log.sh
+++ jenkins/gerrit-bazel-cat-test-log.sh
@@ -7,7 +7,10 @@ then
    echo "See test log below"
    echo "=============================="
    echo ""
-   if [ -f gerrit/polygerrit-ui/karma_test.sh ]
+   if [ -f gerrit/polygerrit-ui/web_test_runner.sh ]
+   then
+     cat $(ls ~/.cache/bazel/_bazel_jenkins/*/execroot/gerrit/bazel-out/*/testlogs/polygerrit-ui/web_test_runner/test.log)
+   elif [ -f gerrit/polygerrit-ui/karma_test.sh ]
    then
      cat $(ls ~/.cache/bazel/_bazel_jenkins/*/execroot/gerrit/bazel-out/*/testlogs/polygerrit-ui/karma_test/test.log)
    else
