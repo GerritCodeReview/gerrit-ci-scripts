ee16859f56e233abd73f98732ba50c328adc7078
Nasser Grainawi <nasser.grainawi@oss.qualcomm.com>
2025-05-30 16:48:47 -0700

    Use Eclipse Adoptium Java packages
    Instead of trying to pick up packages from different Debian releases,
    use the open source and broadly compatible packages published by
    Adoptium (aka Eclipse Temurin) [1]. This also simplifies the JDK 8
    install for the agent-sbt docker image since that was already trying to
    use Adoptium.
    
    The only significant impact is that the install paths are now named
    differently: `/usr/lib/jvm/temurin-<JAVA_VERSION>-jdk-amd64`
    
    [1] https://adoptium.net/installation/linux/
    
    Change-Id: I26305ae38e7656d0137b7ee511d4f761d478ad99

--- jenkins-docker/agent-debian/set-java.sh
+++ jenkins-docker/agent-debian/set-java.sh
@@ -6,10 +6,10 @@ then
   echo ""
   echo "Use    : $0 [<version number> | --branch <branch name>]"
   echo "Example: $0 17"
-  echo "         Java set to: /usr/lib/jvm/java-17-openjdk-amd64/bin/java"
+  echo "         Java set to: /usr/lib/jvm/temurin-17-jdk-amd64/bin/java"
   echo "Providing --branch automatically selects the java version"
   echo "Example: $0 --branch stable-3.11"
-  echo "         Java set to: /usr/lib/jvm/java-21-openjdk-amd64/bin/java"
+  echo "         Java set to: /usr/lib/jvm/temurin-21-jdk-amd64/bin/java"
   exit 1
 fi
 
@@ -33,7 +33,7 @@ else
   JAVA_VERSION=$1
 fi
 
-export JAVA_HOME=/usr/lib/jvm/java-$JAVA_VERSION-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/temurin-$JAVA_VERSION-jdk-amd64
 export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
 
 echo "Java set to: $(which java)"
