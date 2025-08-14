e649cf9f83ca3d50dc27768882899fcba01a1dd4
Daniele Sassoli <danielesassoli@gmail.com>
2025-08-14 15:38:35 +0100

    Printing currently checkout version of Gerrit
    Change-Id: I858fa453bc77fc769639e8b8552e6fd0335bd201

--- vars/pluginPipeline.groovy
+++ vars/pluginPipeline.groovy
@@ -98,6 +98,7 @@ def call(Map parm = [:]) {
                     script { if (buildCheck) { gerritCheck (checks: ["${buildCheck}": 'RUNNING'], url: "${env.BUILD_URL}console") } }
                     sh 'git clone --recursive -b $GERRIT_BRANCH https://gerrit.googlesource.com/gerrit'
                     dir ('gerrit') {
+                        sh "git rev-parse HEAD"
                         sh "cd plugins && ln -s ../../${pluginName} ."
                         sh "if [ -f ../${pluginName}/external_plugin_deps.bzl ]; then cd plugins && ln -sf ../../${pluginName}/external_plugin_deps.bzl .; fi"
                         sh "if [ -f ../${pluginName}/external_package.json ]; then cd plugins && ln -sf ../../${pluginName}/external_package.json package.json; fi"
