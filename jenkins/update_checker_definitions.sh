e4646784fb76af6eb787c091de3de284167ca2e3
Luca Milanesio <luca.milanesio@gmail.com>
2025-09-29 22:22:35 +0100

    Convert the checkers_definitions.txt into a runnable script
    Transform the definition of Gerrit checkers into something
    that can be just executed, making easier to update the checkers
    and avoiding any potential human error.
    
    Change-Id: I139f81b51e47a5ecac750916b29acdc21fbf50ac

--- jenkins/update_checker_definitions.sh
+++ jenkins/update_checker_definitions.sh
@@ -1,54 +1,49 @@
-POST /plugins/checks/checkers/ HTTP/1.0
-  Content-Type: application/json; charset=UTF-8
-  {
-    "uuid": "gerritforge-gcp:rbe-a6a0e4682515f3521897c5f950d1394f4619d928",
+curl -X POST -n -v https://gerrit-review.googlesource.com/a/plugins/checks/checkers/gerritforge-gcp%3Arbe-a6a0e4682515f3521897c5f950d1394f4619d928 \
+  -H 'Content-Type: application/json; charset=UTF-8' \
+-d '{
     "name": "RBE GCP Build/Tests",
     "description": "Builds the code base and executes unit/integration tests on GCP RBE",
     "repository": "gerrit",
     "query": "(not dir:polygerrit-ui) AND (branch:stable-3.10 OR branch:stable-3.11 OR branch:stable-3.12 OR branch:stable-3.13 OR branch:master)",
     "blocking": []
-  }
+  }'
 
-POST /plugins/checks/checkers/ HTTP/1.0
-  Content-Type: application/json; charset=UTF-8
+curl -X POST -n -v https://gerrit-review.googlesource.com/a/plugins/checks/checkers/gerritforge%3Arbe-a6a0e4682515f3521897c5f950d1394f4619d928  \
+  -H 'Content-Type: application/json; charset=UTF-8' -d '
   {
-    "uuid": "gerritforge:rbe-a6a0e4682515f3521897c5f950d1394f4619d928",
     "name": "RBE BB Build/Tests",
     "description": "Builds the code base and executes unit/integration tests on BuildBuddy RBE",
     "repository": "gerrit",
     "query": "(not dir:polygerrit-ui)  AND -age:1w AND (branch:stable-3.10 OR branch:stable-3.11 OR branch:stable-3.12 OR branch:stable-3.13 OR branch:master)",
     "blocking": []
-  }
+  }'
 
-POST /plugins/checks/checkers/ HTTP/1.0
-  Content-Type: application/json; charset=UTF-8
+curl -X POST -n -v https://gerrit-review.googlesource.com/a/plugins/checks/checkers/gerritforge%3Anotedb-a6a0e4682515f3521897c5f950d1394f4619d928  \
+  -H 'Content-Type: application/json; charset=UTF-8' -d '
   {
-    "uuid": "gerritforge:notedb-a6a0e4682515f3521897c5f950d1394f4619d928",
     "name": "Build/Tests",
     "description": "Builds the code base and executes unit/integration tests",
     "repository": "gerrit",
     "query": "(not dir:polygerrit-ui) AND (branch:stable-3.10 OR branch:stable-3.11 OR branch:stable-3.12 OR branch:stable-3.13 OR branch:master)",
     "blocking": []
-  }
+  }'
 
-POST /plugins/checks/checkers/ HTTP/1.0
-  Content-Type: application/json; charset=UTF-8
+curl -X POST -n -v https://gerrit-review.googlesource.com/a/plugins/checks/checkers/gerritforge%3Apolygerrit-a6a0e4682515f3521897c5f950d1394f4619d928  \
+  -H 'Content-Type: application/json; charset=UTF-8' -d '
   {
-    "uuid": "gerritforge:polygerrit-a6a0e4682515f3521897c5f950d1394f4619d928",
     "name": "PolyGerrit UI Tests",
     "description": "Executes unit/integration tests for PolyGerrit UI",
     "repository": "gerrit",
     "query": "(dir:polygerrit-ui OR file:WORKSPACE) AND (branch:stable-3.10 OR branch:stable-3.11 OR branch:stable-3.12 OR branch:stable-3.13 OR branch:master)",
     "blocking": []
-  }
+  }'
 
-POST /plugins/checks/checkers/ HTTP/1.0
-  Content-Type: application/json; charset=UTF-8
+curl -X POST -n -v https://gerrit-review.googlesource.com/a/plugins/checks/checkers/gerritforge%3Acodestyle-a6a0e4682515f3521897c5f950d1394f4619d928  \
+  -H 'Content-Type: application/json; charset=UTF-8' -d '
   {
-    "uuid": "gerritforge:codestyle-a6a0e4682515f3521897c5f950d1394f4619d928",
     "name": "Code Style",
     "description": "Executes Code Style tests",
     "repository": "gerrit",
     "query": "branch:stable-3.10 OR branch:stable-3.11 OR branch:stable-3.12 OR branch:stable-3.13 OR branch:master",
     "blocking": []
-  }
+  }'
