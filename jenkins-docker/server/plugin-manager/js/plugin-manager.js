efa7b95ffa1529c938a8b4513e7d492c551d38bc
Luca Milanesio <luca.milanesio@gmail.com>
2025-10-12 07:18:28 +0100

    Display plugin source URL when hovering the source badge
    Change-Id: Ie9e9f99229d20fa6b9630fd067a30e73646e1d15

--- jenkins-docker/server/plugin-manager/js/plugin-manager.js
+++ jenkins-docker/server/plugin-manager/js/plugin-manager.js
@@ -81,6 +81,7 @@ var app = angular.module('PluginManager', []).controller(
                       currPlugin.sha1 = plugin.sha1;
                       var uiPluginRegex = /(ui-plugin-)(.*)-bazel.*/;
                       var fileEnding = plugin.name.match(uiPluginRegex) ? '.js' : '.jar';
+                      currPlugin.jobUrl = $scope.getBaseUrl() + '/job/' + plugin.name;
                       currPlugin.url = $scope.getBaseUrl() + '/job/' + plugin.name + '/lastSuccessfulBuild/artifact/bazel-bin/plugins/' + pluginName + '/' + pluginName + fileEnding;
                       currPlugin.description = pluginResponse.data.description;
                       currPlugin.source = source;
@@ -98,11 +99,47 @@ var app = angular.module('PluginManager', []).controller(
                     });
                   });
                 }, function errorCallback(response) {
+                  console.error('Failed to fetch plugin build data for ' + pluginId, response);
                 });
       }
 
       plugins.login = function () {
         $window.location.href = 'https://gerrit-ci.gerritforge.com/securityRealm/commenceLogin?from=%2F';
+      };
+
+      $scope.showRepoStatus = function(e, pluginId, pluginJobUrl) {
+        $timeout(function() {
+          $http.get(pluginJobUrl + '/lastSuccessfulBuild/api/json', plugins.httpConfig)
+            .then(
+              function successCallback(response) {
+                var repoStatusPopup = document.getElementById('repo-status-popup-' + pluginId);
+                var repoStatusPopupAnchor = document.getElementById('repo-status-popup-a-' + pluginId);
+                if (!repoStatusPopup) return;
+
+                var scmAction = response.data.actions.filter(function(action) {
+                  return action._class == "hudson.plugins.git.util.BuildData";
+                });
+                if (!scmAction || scmAction.length == 0) return;
+
+                var sourceUrl = scmAction[0].remoteUrls.filter(function(url) {
+                      return url.indexOf("gerrit.googlesource.com/a/gerrit") === -1;
+                    });
+                if (!sourceUrl || sourceUrl.length == 0) return;
+
+                repoStatusPopup.style.display = 'block';
+                repoStatusPopupAnchor.href = sourceUrl[0].replace("/a", "");
+              }, function errorCallback(response) {}
+            );
+        }, 500);
+      };
+
+      $scope.hideRepoStatus = function(pluginId) {
+        var repoStatusPopup = document.getElementById('repo-status-popup-' + pluginId);
+        if (repoStatusPopup) {
+          $timeout(function() {
+            repoStatusPopup.style.display = 'none';
+          }, 1000);
+        }
       };
 
       $scope.refreshAvailable();
