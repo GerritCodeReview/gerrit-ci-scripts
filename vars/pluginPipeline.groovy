89b8f6a481755f6bf79fd20b32137d3620261abb
Daniele Sassoli <danielesassoli@gmail.com>
2025-12-02 14:43:05 +0000

    Correctly set GERRIT_PATCHSET_NUMBER
    Change-Id: I071d4e28d5baf8dcaa9427daf2ff923aa1fd7e1c

--- vars/pluginPipeline.groovy
+++ vars/pluginPipeline.groovy
@@ -46,7 +46,7 @@ def call(Map parm = [:]) {
     def githubBaseUrl = ""
 
     echo "Starting pipeline for plugin '${pluginName}'" + (formatCheck ? " formatCheckId=${formatCheck}" : '') + (buildCheck ? " buildCheckId=${buildCheck}" : '')
-    echo "Change : ${env.GERRIT_CHANGE_NUMBER}/${GERRIT_PATCHSET_NUMBER} '${env.GERRIT_CHANGE_SUBJECT}'"
+    echo "Change : ${env.GERRIT_CHANGE_NUMBER}/${env.GERRIT_PATCHSET_NUMBER} '${env.GERRIT_CHANGE_SUBJECT}'"
     echo "Change URL: ${env.GERRIT_CHANGE_URL}"
 
     pipeline {
