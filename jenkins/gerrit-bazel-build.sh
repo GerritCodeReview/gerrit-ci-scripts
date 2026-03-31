6c5ce1535a470505e17b4b9b9c360ba2cb8bbd6b
David Ostrovsky <david@ostrovsky.org>
2026-03-31 22:14:55 +0200

    Bazel: Remove deprecated 'bazel sync' from CI scripts
    Bazel 9 removed the 'sync' command, causing CI failures with:
      "Command 'sync' not found"
    
    The sync step was previously used as a workaround to prefetch npm
    repositories for PolyGerrit, but this is no longer necessary as Bazel
    automatically fetches external dependencies during build/test.
    
    Remove the obsolete command to restore compatibility with Bazel 9.
    
    Change-Id: Ia0490f3ecca3196dc9ed7cc4786dda76f311650d

--- jenkins/gerrit-bazel-build.sh
+++ jenkins/gerrit-bazel-build.sh
@@ -19,11 +19,6 @@ echo '----------------------------------------------'
 java -fullversion
 bazelisk version
 
-# Whilst all the rest of Gerrit is able to automatically sync the Bazel repositories
-# the PolyGerrit part fails to do so when the working directory is replaced with a
-# fresh clone from the remote Git repository
-bazelisk sync --only=npm --only=tools_npm --only=ui_npm --only=plugins_npm
-
 if [[ "$MODE" == *"rbe"* ]]
 then
   bazelisk build --config=remote_bb --jobs=50 --remote_header=x-buildbuddy-api-key=$BB_API_KEY plugins:core release api
