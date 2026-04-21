76529a5508d4e5e7241699534916ffd31eca81f2
Antonio Barone <syntonyze@gmail.com>
2026-04-21 19:17:22 +0200

    Fix typo when retrieving project info for GH plugins
    This caused the $plugin.json file containing an error message rather
    than the project description
    
    Change-Id: I8f6a80b8210ad2203a95c8e25b092d65eb3e10a0

--- jenkins/gerrit-bazel-gh-get-plugin-info.sh
+++ jenkins/gerrit-bazel-gh-get-plugin-info.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-curl https://review.gerrithub.io/projects/{organization}%2{repo}/config | tail -1 \
+curl https://review.gerrithub.io/projects/{organization}%2F{repo}/config | tail -1 \
      > bazel-bin/plugins/{name}/{name}.json
 
