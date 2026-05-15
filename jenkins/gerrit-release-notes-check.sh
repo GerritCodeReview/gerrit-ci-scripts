fdfc7d09ef5842d0f6dc6060f2a94a36cb9d9804
Adithya Chakilam <achakilam@nvidia.com>
2026-05-15 16:06:48 -0500

    Add Release-Notes footer check to Gerrit codestyle job
    All changes to the Gerrit project require a Release-Notes footer.
    Add a pre-check that fails the codestyle build if the commit message
    is missing it, prompting contributors to either describe the change
    or use 'Release-Notes: skip'.
    
    Change-Id: I35fdc13dd72ed54b87c3b7c51bfeaebf011235ef

---
+++ jenkins/gerrit-release-notes-check.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -xe
+#
+# Copyright (C) 2026 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+pushd gerrit
+
+echo 'Checking for Release-Notes footer in commit message...'
+if ! git log -1 --format=%B HEAD | grep -qE '^Release-Notes:'; then
+  echo "ERROR: Commit message is missing a 'Release-Notes:' footer."
+  echo "  Release-Notes: <single sentence description of the change>"
+  echo ""
+  echo "If your change isn't worth mentioning in the release notes, use:"
+  echo "  Release-Notes: skip"
+  exit 1
+fi
+
+echo 'Release-Notes footer check passed.'
+popd
